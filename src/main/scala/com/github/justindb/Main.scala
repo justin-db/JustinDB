94b2ef89957b88351862af7143f382f156a8f03b
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-07-03 15:23:19 +0200

    add test to Consistent Hashing

--- src/main/scala/com/github/justindb/Main.scala
+++ src/main/scala/com/github/justindb/Main.scala
@@ -6,33 +6,4 @@ import scala.util.Random
 
 object Main extends App {
 
-  ConsistentHashing.addNodes(
-    realNodes = 5,
-    virtualNodesPerNode = 2000
-  )
-
-  for {
-    nodeId <- ConsistentHashing.getNodeId("aaaa")
-  } yield {
-    val node = ConsistentHashing.ring(nodeId)
-    println(node)
-    println(node.store)
-  }
-
-  Node.addRecord(
-    key = "aaaa",
-    v = TextRecord("pierwszy rekord w bazie")
-  )
-  Node.addRecord(
-    key = "bbbb",
-    v = TextRecord("drugi rekord w bazie")
-  )
-  Node.addRecord(
-    key = "cccc",
-    v = LongRecord(9999L)
-  )
-
-  assert(Node.findRecord("aaaa").get == TextRecord("pierwszy rekord w bazie"))
-  assert(Node.findRecord("cccc").get == LongRecord(9999L))
-
 }
