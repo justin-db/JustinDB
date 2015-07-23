631c7d3ab385eb967186cb014e14b510fd2f0d6d
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-07-23 21:45:28 +0200

    delete file with defined Roles (not used anymore)

--- src/main/scala/com/justindb/Roles.scala
+++
@@ -1,9 +0,0 @@
-package com.justindb
-
-sealed trait Role
-case object RingRole extends Role
-case object NodeRole extends Role
-
-object Role {
-  implicit def toString(r: Role): String = r.toString
-}
