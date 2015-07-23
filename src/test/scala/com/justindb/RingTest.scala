28609353126954cd683f5171e0c969aabee407c2
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-07-23 20:53:18 +0200

    test that Ring allow adding new Nodes to it

--- src/test/scala/com/justindb/RingTest.scala
+++ src/test/scala/com/justindb/RingTest.scala
@@ -1,7 +1,7 @@
 package com.justindb
 
 import org.scalatest._
-import com.justindb.Ring
+import com.justindb.{Ring, Node, Hash}
 
 class RingTest extends FlatSpec with Matchers {
 
@@ -9,6 +9,16 @@ class RingTest extends FlatSpec with Matchers {
     val ring = new Ring
 
     ring.underlying should have size 0
+  }
+
+  it should "allow to save Node with combined Hash" in {
+    var ring = new Ring
+
+    val node = Node(Key(value = "random-key"), underlyingActor = null)
+    val nodeHash = Hash(500)
+    ring = Ring(ring.underlying + ((nodeHash -> node)))
+
+    ring.underlying should have size 1
   }
 
 }
