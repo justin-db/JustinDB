7b5f73ea5b49f70c068d00bd51a66146ca703cd5
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-23 23:38:24 +0200

    test that addition operator of Digest class works

---
+++ justin-merkle-trees/src/test/scala/justin/merkle_trees/DigestTest.scala
@@ -0,0 +1,17 @@
+package justin.merkle_trees
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class DigestTest extends FlatSpec with Matchers {
+
+  behavior of "Digest"
+
+  it should "define addition operator" in {
+    val digest1 = Digest(Array[Byte](1,2,3))
+    val digest2 = Digest(Array[Byte](4,5,6))
+
+    val result = digest1 + digest2
+
+    result.hash.deep shouldBe Array[Byte](1,2,3,4,5,6)
+  }
+}
