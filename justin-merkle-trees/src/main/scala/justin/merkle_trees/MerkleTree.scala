d9b92b2899cce2d64d16820be9a9bbe5f159e36d
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-23 23:42:51 +0200

    clean up MerkleTree impl

--- justin-merkle-trees/src/main/scala/justin/merkle_trees/MerkleTree.scala
+++ justin-merkle-trees/src/main/scala/justin/merkle_trees/MerkleTree.scala
@@ -9,23 +9,22 @@ case class MerkleLeaf(hash: Digest) extends MerkleTree
 object MerkleTree {
 
   def unapply(data: Seq[Block])(implicit ev: MerkleDigest[Block]): Option[MerkleTree] = {
-    def blockToLeaf(b: Block) = MerkleLeaf(ev.digest(b))
-
     val leafs = data.map(blockToLeaf)
-    Try(buildTree(leafs)).toOption
+    buildTree(leafs).toOption
   }
 
-  private def buildTree(leafs: Seq[MerkleLeaf])(implicit ev: MerkleDigest[Block]): MerkleTree = {
-    def merge(n1: MerkleTree, n2: MerkleTree) = MerkleHashNode(n1.hash + n2.hash, n1, n2)
+  private def blockToLeaf(b: Block)(implicit ev: MerkleDigest[Block]) = MerkleLeaf(ev.digest(b))
 
+  private def buildTree(leafs: Seq[MerkleLeaf])(implicit ev: MerkleDigest[Block]) = Try {
     var trees: Seq[MerkleTree] = leafs
 
     while (trees.length > 1) {
       trees = trees.grouped(2)
-        .map { case (n1 :: n2 :: Nil) => merge(n1, n2) }
+        .map { case (n1 :: n2 :: Nil) => mergeTrees(n1, n2) }
         .toSeq
     }
-
     trees.head
   }
+
+  private def mergeTrees(n1: MerkleTree, n2: MerkleTree) = MerkleHashNode(n1.hash + n2.hash, n1, n2)
 }
