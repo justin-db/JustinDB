4725aadb01c09a2c771adf05f06a4722c2000a97
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-24 00:04:20 +0200

    Merkle Tree test - "have a different top hash"

--- justin-merkle-trees/src/test/scala/justin/merkle_trees/MerkleTreeTest.scala
+++ justin-merkle-trees/src/test/scala/justin/merkle_trees/MerkleTreeTest.scala
@@ -20,7 +20,29 @@ class MerkleTreeTest extends FlatSpec with Matchers {
       Array[Byte](10,11,12)
     )
 
-    MerkleTree.unapply(data) shouldBe MerkleTree.unapply(data2)
+    val hash1 = MerkleTree.unapply(data)(MerkleDigest.CRC32).get.hash
+    val hash2 = MerkleTree.unapply(data2)(MerkleDigest.CRC32).get.hash
+
+    hash1.hash.deep shouldBe hash2.hash.deep
   }
 
+  it should "have a different top hash" in {
+    val data: Seq[Block] = Seq(
+      Array[Byte](1,2,3),
+      Array[Byte](4,5,6),
+      Array[Byte](7,8,9),
+      Array[Byte](10,11,12)
+    )
+    val data2: Seq[Block] = Seq(
+      Array[Byte](1,2,3),
+      Array[Byte](4,5,6),
+      Array[Byte](9,8,7),
+      Array[Byte](12,11,10)
+    )
+
+    val hash1 = MerkleTree.unapply(data)(MerkleDigest.CRC32).get.hash
+    val hash2 = MerkleTree.unapply(data2)(MerkleDigest.CRC32).get.hash
+
+    hash1.hash.deep should not be hash2.hash.deep
+  }
 }
