ef85a1430e9188e3f14bb7bf6755a9f915b0c317
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-24 14:19:57 +0200

    define max method in Counter's companion object for choosing counter with bigger value

--- justin-vector-clocks/src/main/scala/justin/vector_clocks/VectorClock.scala
+++ justin-vector-clocks/src/main/scala/justin/vector_clocks/VectorClock.scala
@@ -7,6 +7,12 @@ case class Counter(value: Int) extends AnyVal {
   def addOne: Counter = this.copy(value = value + 1)
 }
 
+object Counter {
+  def max(c1: Counter, c2: Counter): Counter = {
+    Counter(scala.math.max(c1.value, c2.value))
+  }
+}
+
 case class VectorClock(private val clock: Map[VectorId, Counter])
 
 object VectorClock {
@@ -24,7 +30,7 @@ object VectorClock {
     val mergedClocks = vc1.clock ++ vc2.clock
 
     val mergedCounter = (vc1.clock.get(receiverId), vc2.clock.get(receiverId)) match {
-      case (Some(counter1), Some(counter2)) => Counter(scala.math.max(counter1.value, counter2.value))
+      case (Some(counter1), Some(counter2)) => Counter.max(counter1, counter2)
       case (None, Some(counter2))           => counter2
       case (Some(counter1), None)           => counter1
       case (None, None)                     => Counter(0)
