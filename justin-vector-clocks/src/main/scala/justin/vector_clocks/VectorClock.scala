4ec92cedc6b520e8ffdc53eda6e55c61529b95e5
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-24 12:40:06 +0200

    increase counter method

--- justin-vector-clocks/src/main/scala/justin/vector_clocks/VectorClock.scala
+++ justin-vector-clocks/src/main/scala/justin/vector_clocks/VectorClock.scala
@@ -10,4 +10,13 @@ case class VectorClock(private val clock: Map[VectorId, Counter])
 object VectorClock {
 
   def empty(id: UUID): VectorClock = VectorClock(Map(VectorId(id) -> Counter(0)))
+
+  def increase(vc: VectorClock, id: VectorId): Option[VectorClock] = {
+    for {
+      keyCounter <- vc.clock.get(id)
+      updatedCounter = Counter(keyCounter.value + 1)
+    } yield {
+      VectorClock(vc.clock + (id -> updatedCounter))
+    }
+  }
 }
