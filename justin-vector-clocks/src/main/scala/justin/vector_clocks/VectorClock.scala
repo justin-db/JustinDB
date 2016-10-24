db4cfa4728f97404b5d67528670f7b98ca8bc778
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-24 13:04:51 +0200

    create a key when key was not found when trying to increase corresponding counter

--- justin-vector-clocks/src/main/scala/justin/vector_clocks/VectorClock.scala
+++ justin-vector-clocks/src/main/scala/justin/vector_clocks/VectorClock.scala
@@ -3,7 +3,9 @@ package justin.vector_clocks
 import java.util.UUID
 
 case class VectorId(uuid: UUID) extends AnyVal
-case class Counter(value: Int) extends AnyVal
+case class Counter(value: Int) extends AnyVal {
+  def addOne: Counter = this.copy(value = value + 1)
+}
 
 case class VectorClock(private val clock: Map[VectorId, Counter])
 
@@ -11,13 +13,9 @@ object VectorClock {
 
   def empty(id: UUID): VectorClock = VectorClock(Map(VectorId(id) -> Counter(0)))
 
-  def increase(vc: VectorClock, id: VectorId): Option[VectorClock] = {
-    for {
-      keyCounter <- vc.clock.get(id)
-      updatedCounter = Counter(keyCounter.value + 1)
-    } yield {
-      VectorClock(vc.clock + (id -> updatedCounter))
-    }
+  def increase(vc: VectorClock, id: VectorId): VectorClock = {
+    val updatedCounter = vc.clock.getOrElse(id, Counter(0)).addOne
+    VectorClock(vc.clock + (id -> updatedCounter))
   }
 
   def merge(receiverId: VectorId, vc1: VectorClock, vc2: VectorClock): VectorClock = {
