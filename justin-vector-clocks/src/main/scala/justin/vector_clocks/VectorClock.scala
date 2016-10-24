605900512709c35b666a6ed5e568656579874710
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-24 12:39:56 +0200

    empty method for creation of empty VectorClock instance with passed id

--- justin-vector-clocks/src/main/scala/justin/vector_clocks/VectorClock.scala
+++ justin-vector-clocks/src/main/scala/justin/vector_clocks/VectorClock.scala
@@ -6,3 +6,8 @@ case class VectorId(uuid: UUID) extends AnyVal
 case class Counter(value: Int) extends AnyVal
 
 case class VectorClock(private val clock: Map[VectorId, Counter])
+
+object VectorClock {
+
+  def empty(id: UUID): VectorClock = VectorClock(Map(VectorId(id) -> Counter(0)))
+}
