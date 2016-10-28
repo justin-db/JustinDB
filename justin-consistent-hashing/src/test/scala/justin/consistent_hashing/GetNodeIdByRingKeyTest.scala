8e264981011d8fc363f1610cc1fc9704a08df33c
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-28 21:19:15 +0200

    behavior of "Get NodeId By RingKey"

--- justin-consistent-hashing/src/test/scala/justin/consistent_hashing/GetNodeIdByRingKeyTest.scala
+++ justin-consistent-hashing/src/test/scala/justin/consistent_hashing/GetNodeIdByRingKeyTest.scala
@@ -1,12 +1,17 @@
 package justin.consistent_hashing
 
+import java.util.UUID
+
 import org.scalatest.{FlatSpec, Matchers}
 
 class GetNodeIdByRingKeyTest extends FlatSpec with Matchers {
 
-  behavior of "GetNodeIdByKey"
+  behavior of "Get NodeId By RingKey"
 
-  it should "" in {}
+  it should "assign Id to correct NodeId" in {
+    val ring = NodeMapRing.apply(N = 3, S = 30)
+    val getByKey = new GetNodeIdByRingKey(ring, uuid => 30)
 
-
+    getByKey(UUID.randomUUID()) shouldBe Some(NodeId(0))
+  }
 }
