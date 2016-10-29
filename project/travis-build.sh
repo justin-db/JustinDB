8ece8cdbf99586244a18b6160e15b5a359b6fbe4
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-30 00:03:48 +0200

    permission travis build

---
+++ project/travis-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+cd `dirname $0`/..
+
+if [ -z "$MAIN_SCALA_VERSION" ]; then
+    >&2 echo "Environment MAIN_SCALA_VERSION is not set. Check .travis.yml."
+    exit 1
+elif [ -z "$TRAVIS_SCALA_VERSION" ]; then
+    >&2 echo "Environment TRAVIS_SCALA_VERSION is not set."
+    exit 1
+else
+    echo
+    echo "TRAVIS_SCALA_VERSION=$TRAVIS_SCALA_VERSION"
+    echo "MAIN_SCALA_VERSION=$MAIN_SCALA_VERSION"
+fi
+
+function buildJVM {
+    INIT="clean"
+    COMPILE="compile"
+    TEST="test"
+
+    COMMAND=";$INIT;$COMPILE;$TEST"
+     echo
+     echo "Executing JVM tests (with coverage): sbt -Dsbt.profile=coverage $COMMAND"
+     echo
+     sbt -Dsbt.profile=coverage "$COMMAND"
+}
+
+# Execute everything
+buildJVM
