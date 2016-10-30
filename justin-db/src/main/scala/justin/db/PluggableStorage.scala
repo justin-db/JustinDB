e00bc2d841775dc8c950dfb464ea9b4675b3432d
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-30 21:42:15 +0100

    in-memory implementation of pluggable-storage interface

--- justin-db/src/main/scala/justin/db/PluggableStorage.scala
+++ justin-db/src/main/scala/justin/db/PluggableStorage.scala
@@ -1,6 +1,15 @@
 package justin.db
 
+import scala.collection.mutable
+
 trait PluggableStorage {
   def get(key: String): Option[String]
   def put(key: String, value: String): Unit
+}
+
+class InMemStorage extends PluggableStorage {
+  private var values = mutable.Map.empty[String, String]
+
+  override def get(key: String): Option[String] = values.get(key)
+  override def put(key: String, value: String): Unit = { values = values + ((key,value)) }
 }
