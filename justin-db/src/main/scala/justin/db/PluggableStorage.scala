a8b5f7bf70f897276fddfe76d10c6e3bef3ed985
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-10-31 12:03:36 +0100

    file-per-key pluggable storage impl

--- justin-db/src/main/scala/justin/db/PluggableStorage.scala
+++ justin-db/src/main/scala/justin/db/PluggableStorage.scala
@@ -1,6 +1,5 @@
 package justin.db
 
-import scala.collection.mutable
 
 trait PluggableStorage {
   def get(key: String): Option[String]
@@ -8,8 +7,41 @@ trait PluggableStorage {
 }
 
 class InMemStorage extends PluggableStorage {
+  import scala.collection.mutable
+
   private var values = mutable.Map.empty[String, String]
 
   override def get(key: String): Option[String] = values.get(key)
   override def put(key: String, value: String): Unit = { values = values + ((key,value)) }
+}
+
+class FilePerKeyStorage extends PluggableStorage {
+  import java.io.{File, PrintWriter}
+  import FilePerKeyStorage._
+
+  override def get(key: String): Option[String] = {
+    val source = scala.io.Source.fromFile(fileNameWithTxtExtenstion(key))
+    try {
+      Option(source.getLines().mkString)
+    } catch {
+      case ex: Exception => None // TODO: log about failure
+    } finally {
+     source.close()
+    }
+  }
+
+  override def put(key: String, value: String): Unit = {
+    val saver = new PrintWriter(new File(fileNameWithTxtExtenstion(key)))
+    try {
+      saver.println(value)
+    } catch {
+      case ex: Exception => // TODO: log about failure
+    } finally {
+      saver.close()
+    }
+  }
+}
+
+object FilePerKeyStorage {
+  def fileNameWithTxtExtenstion(fileName: String) = s"$fileName.txt"
 }
