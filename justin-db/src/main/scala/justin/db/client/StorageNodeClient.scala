ba2d36397ddb4690b3137479bd409d5501df34d8
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-02 20:25:24 +0100

    basic client interface of StorageNode

---
+++ justin-db/src/main/scala/justin/db/client/StorageNodeClient.scala
@@ -0,0 +1,26 @@
+package justin.db.client
+
+import java.util.UUID
+
+import scala.concurrent.Future
+
+trait StorageNodeClient {
+  def get(id: UUID, r: ReadFactor): Future[GetValueResponse]
+  def write(value: String, w: WriteFactor): Future[GetValueResponse]
+}
+
+sealed trait GetValueResponse
+object GetValueResponse {
+  case class Found(value: String) extends GetValueResponse
+  case object NotFound extends GetValueResponse
+  case class Failure(error: String) extends GetValueResponse
+}
+
+sealed trait WriteValueResponse
+object WriteValueResponse {
+  case object Success extends GetValueResponse
+  case class Failure(error: String) extends GetValueResponse
+}
+
+case class ReadFactor(r: Int) extends AnyVal
+case class WriteFactor(w: Int) extends AnyVal
