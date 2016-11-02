80354b0313200f25b52bfaf4adb89697a0b2b6bf
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-02 08:50:57 +0100

    build preference list

--- justin-db/src/main/scala/justin/db/StorageNode.scala
+++ justin-db/src/main/scala/justin/db/StorageNode.scala
@@ -5,6 +5,20 @@ import justin.db.storage.PluggableStorage
 
 case class StorageNodeId(id: Int) extends AnyVal
 
-class StorageNode(nodeId: StorageNodeId, storage: PluggableStorage) extends Actor {
+class StorageNode(nodeId: StorageNodeId, preferenceList: List[StorageNodeId], storage: PluggableStorage) extends Actor {
   override def receive: Receive = ???
 }
+
+object StorageNode {
+  def buildPreferenceList(baseId: StorageNodeId, replicationFactor: ReplicationFactor): List[StorageNodeId] = {
+    val floor   = baseId.id + 1
+    val ceiling = baseId.id + replicationFactor.n
+
+    (floor to ceiling)
+      .filterNot(_ == baseId.id)
+      .map(StorageNodeId)
+      .toList
+  }
+}
+
+case class ReplicationFactor(n: Int) extends AnyVal
