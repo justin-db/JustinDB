a29ce9ed0cf7e14d43af3975b22e56843e868fee
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-03 16:45:58 +0100

    StorageNoce actor - handle GetValue and PutValue messages

--- justin-db/src/main/scala/justin/db/StorageNode.scala
+++ justin-db/src/main/scala/justin/db/StorageNode.scala
@@ -1,8 +1,11 @@
 package justin.db
 
+import java.util.UUID
+
 import akka.actor.{Actor, Props}
 import akka.cluster.Cluster
 import akka.cluster.ClusterEvent.MemberUp
+import justin.db.StorageNode.{GetValue, PutValue}
 import justin.db.storage.PluggableStorage
 
 case class StorageNodeId(id: Int) extends AnyVal
@@ -15,11 +18,17 @@ class StorageNode(nodeId: StorageNodeId, storage: PluggableStorage) extends Acto
   override def postStop(): Unit = cluster.unsubscribe(self)
 
   override def receive: Receive = {
-    case t => println("msg: " + t)
+    case GetValue(id)        => sender() ! storage.get(id.toString)
+    case PutValue(id, value) => storage.put(id.toString, value)
+    case t                   => println("msg: " + t)
   }
 }
 
 object StorageNode {
+
+  sealed trait StorageNodeReq
+  case class GetValue(id: UUID) extends StorageNodeReq
+  case class PutValue(id: UUID, value: String) extends StorageNodeReq
 
   def role: String = "StorageNode"
 
