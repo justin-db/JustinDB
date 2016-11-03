4e5ec3f30e1a7f278cf8ed1f060f5362de7ec595
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-03 16:46:50 +0100

    http-storage-node-client: ask StorageNode actor for value

--- justin-db/client/http/src/main/scala/justin/db/client/HttpStorageNodeClient.scala
+++ justin-db/client/http/src/main/scala/justin/db/client/HttpStorageNodeClient.scala
@@ -1,12 +1,27 @@
 package justin.db.client
 
 import java.util.UUID
+
 import akka.actor.ActorRef
+import akka.pattern.ask
+import akka.util.Timeout
+
+import justin.db.StorageNode.GetValue
+
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 
-// TODO
 class HttpStorageNodeClient(storageNodeActor: ActorRef)(implicit ex: ExecutionContext) extends StorageNodeClient {
-  override def get(id: UUID, r: ReadFactor): Future[GetValueResponse] = ???
+
+  implicit val timeout = Timeout(5.seconds) // TODO: tune this value
+
+  // TODO: make it fully non-blocking
+  override def get(id: UUID, r: ReadFactor): Future[GetValueResponse] = {
+    (storageNodeActor ? GetValue(id)).mapTo[Option[String]].map {
+      case Some(value) => GetValueResponse.Found(value)
+      case None        => GetValueResponse.NotFound
+    }.recover { case _ => GetValueResponse.Failure(s"[Failure] Couldn't get value with id ${id.toString}") }
+  }
 
   override def write(value: String, w: WriteFactor): Future[GetValueResponse] = ???
 }
