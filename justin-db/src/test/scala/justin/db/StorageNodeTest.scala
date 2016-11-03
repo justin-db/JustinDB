903f452d8db8ebda79b15eb68d7f3b6668ae19ca
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-03 13:00:12 +0100

    test StorageNode's \"name\" method

--- justin-db/src/test/scala/justin/db/StorageNodeTest.scala
+++ justin-db/src/test/scala/justin/db/StorageNodeTest.scala
@@ -29,4 +29,8 @@ class StorageNodeTest extends FlatSpec with Matchers {
   it should "has defined role \"StorageNode\"" in {
     StorageNode.role shouldBe "StorageNode"
   }
+
+  it should "define name for actor with combination of its id" in {
+    StorageNode.name(StorageNodeId(10)) shouldBe "id-10"
+  }
 }
