09801368b25f321ef8a6424b480290f215b5a3c5
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-04 14:55:50 +0100

    change defs to lazy vals

--- justin-consistent-hashing/src/main/scala/justin/consistent_hashing/NodeMapRing.scala
+++ justin-consistent-hashing/src/main/scala/justin/consistent_hashing/NodeMapRing.scala
@@ -4,17 +4,19 @@ import justin.consistent_hashing.NodeMapRing.RingKey
 
 case class NodeId(id: Int) extends AnyVal
 
-// TODO: change def's to lazy vals
 case class NodeMapRing(private val ring: Map[RingKey, NodeId]) {
   def getByKey(key: RingKey): Option[NodeId] = ring.get(key)
-  def size: Int = ring.size
-  def nodesId: Set[NodeId] = ring.values.toSet
-  def swap: Map[NodeId, List[RingKey]] = {
+
+  lazy val size: Int = ring.size
+
+  lazy val nodesId: Set[NodeId] = ring.values.toSet
+
+  lazy val swap: Map[NodeId, List[RingKey]] = {
     val initial = Map.empty[NodeId, List[RingKey]]
     ring.foldLeft(initial) { case (finalMap, (ringKey, nodeId)) =>
       val currentNodeVals = finalMap.getOrElse(nodeId, List.empty[RingKey])
-      val finalVals = ringKey :: currentNodeVals
-      finalMap ++ Map(nodeId -> finalVals.sorted)
+      val finalVals = (ringKey :: currentNodeVals).sorted
+      finalMap + (nodeId -> finalVals)
     }
   }
 }
