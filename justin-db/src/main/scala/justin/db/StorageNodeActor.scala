dfae0ac239d737e873912588d9502ec2f477bb17
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-07 19:22:44 +0100

    register member clusters

--- justin-db/src/main/scala/justin/db/StorageNodeActor.scala
+++ justin-db/src/main/scala/justin/db/StorageNodeActor.scala
@@ -2,26 +2,43 @@ package justin.db
 
 import java.util.UUID
 
-import akka.actor.{Actor, Props}
-import akka.cluster.Cluster
-import akka.cluster.ClusterEvent.MemberUp
-import justin.consistent_hashing.Ring
-import justin.db.StorageNodeActor.{GetValue, PutValue}
+import akka.actor.{Actor, ActorRef, Props, RootActorPath}
+import akka.cluster.{Cluster, Member, MemberStatus}
+import akka.cluster.ClusterEvent.{CurrentClusterState, MemberUp}
+import justin.consistent_hashing.{GetNodeIdByPartitionId, Ring}
+import justin.db.StorageNodeActor.{GetValue, NodeRegistration, PutValue}
 import justin.db.storage.PluggableStorage
 
 case class StorageNodeActorId(id: Int) extends AnyVal
 
 class StorageNodeActor(nodeId: StorageNodeActorId, storage: PluggableStorage, ring: Ring) extends Actor {
 
-  val cluster = Cluster(context.system)
+  val cluster        = Cluster(context.system)
+  var clusterMembers = Map.empty[StorageNodeActorId, ActorRef]
 
   override def preStart(): Unit = cluster.subscribe(self, classOf[MemberUp])
   override def postStop(): Unit = cluster.unsubscribe(self)
 
+  val nodeFromRing = new GetNodeIdByPartitionId(ring)
+
   override def receive: Receive = {
-    case GetValue(id)        => sender() ! storage.get(id.toString)
-    case PutValue(id, value) => storage.put(id.toString, value); sender() ! "ack" // TODO: send back more meaningful value
-    case t                   => println("msg: " + t)
+    case GetValue(id) =>
+      sender() ! storage.get(id.toString)
+    case pv @ PutValue(valueId, data) =>
+      sender() ! "ack"
+    case state: CurrentClusterState =>
+      state.members.filter(_.status == MemberStatus.Up) foreach register
+    case NodeRegistration(senderNodeId) if !clusterMembers.contains(senderNodeId) =>
+      clusterMembers = clusterMembers + (senderNodeId -> sender())
+      sender() ! NodeRegistration(nodeId)
+    case MemberUp(m) => register(m)
+    case t => println("not handled msg: " + t)
+  }
+
+  private def register(member: Member) = {
+    val rootActorPath = RootActorPath(member.address)
+    val name = if(nodeId.id == 1) "id-0" else "id-1"
+    context.actorSelection(rootActorPath / "user" / s"$name") ! NodeRegistration(nodeId)
   }
 }
 
@@ -30,6 +47,7 @@ object StorageNodeActor {
   sealed trait StorageNodeReq
   case class GetValue(id: UUID) extends StorageNodeReq
   case class PutValue(id: UUID, value: String) extends StorageNodeReq
+  case class NodeRegistration(nodeId: StorageNodeActorId) extends StorageNodeReq
 
   def role: String = "StorageNode"
 
