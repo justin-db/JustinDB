b38b6b367b10dc25ca33bcb5bfb2989a04337bf1
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-08 16:16:33 +0100

    build preference list using knowledge of ring's size

--- justin-db/src/main/scala/justin/db/replication/BuildPreferenceList.scala
+++ justin-db/src/main/scala/justin/db/replication/BuildPreferenceList.scala
@@ -1,16 +1,12 @@
 package justin.db.replication
 
+import justin.consistent_hashing.RingSize
 import justin.db.StorageNodeActorId
 
-// TODO: preference list should take into account how Ring actually is build (current version is rather naive)
 object BuildPreferenceList {
-  def apply(baseId: StorageNodeActorId, n: N): List[StorageNodeActorId] = {
-    val floor   = baseId.id + 1
-    val ceiling = baseId.id + n.n
-
-    (floor to ceiling)
-      .filterNot(_ == baseId.id)
-      .map(StorageNodeActorId)
+  def apply(baseId: StorageNodeActorId, n: N, ringSize: RingSize): List[StorageNodeActorId] = {
+    (0 until n.n)
+      .map(idx => StorageNodeActorId((baseId.id + idx) % ringSize.size))
       .toList
   }
 }
