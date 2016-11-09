15f7073e7da0d14aad498bd0f75c2bf016164727
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-09 17:52:12 +0100

    remove GetNodeIdByUUID class (and its test's suite)

--- justin-consistent-hashing/src/main/scala/justin/consistent_hashing/UUID2PartitionId.scala
+++ justin-consistent-hashing/src/main/scala/justin/consistent_hashing/UUID2PartitionId.scala
@@ -6,7 +6,3 @@ import justin.consistent_hashing.Ring.RingPartitionId
 class UUID2PartitionId(ringSize: RingSize) extends (UUID => Ring.RingPartitionId) {
   override def apply(id: UUID): RingPartitionId = scala.math.abs(id.hashCode()) % ringSize.size
 }
-
-class GetNodeIdByUUID(ring: Ring, UUID2PartitionId: UUID => RingPartitionId) extends (UUID => Option[NodeId]) {
-  override def apply(id: UUID): Option[NodeId] = ring.getNodeId(UUID2PartitionId(id))
-}
