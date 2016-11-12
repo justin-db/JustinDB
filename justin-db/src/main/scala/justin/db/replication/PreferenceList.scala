cc6b7fdd2130a44a746d08abdc21fa025dcabdf2
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-12 09:40:36 +0100

    build preference list of node ids

--- justin-db/src/main/scala/justin/db/replication/PreferenceList.scala
+++ justin-db/src/main/scala/justin/db/replication/PreferenceList.scala
@@ -1,12 +1,17 @@
 package justin.db.replication
 
 import justin.db.consistent_hashing.Ring.RingPartitionId
-import justin.db.consistent_hashing.RingSize
+import justin.db.consistent_hashing.{NodeId, Ring}
 
 object PreferenceList {
-  def apply(baseId: RingPartitionId, n: N, ringSize: RingSize): List[RingPartitionId] = {
-    (0 until n.n)
-      .map(idx => (baseId + idx) % ringSize.size)
-      .toList
+  def apply(baseId: RingPartitionId, n: N, ring: Ring): List[NodeId] = {
+    val coordinatorNodeId = ring.getNodeId(baseId)
+
+    val maxPartitionId = baseId + n.n - 1
+    val restNodesIds   = (baseId until maxPartitionId).toList.map(getNextNodeId(ring))
+
+    (coordinatorNodeId :: restNodesIds).flatten
   }
+
+  private def getNextNodeId(ring: Ring) = ring.nextPartitionId _ andThen ring.getNodeId _
 }
