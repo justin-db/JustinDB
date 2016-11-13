5b665c229b4f29727f69ddae00b2d46f8a59e6f0
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-13 19:37:36 +0100

    expect whole Ring value instead of its size only

--- justin-db/src/main/scala/justin/db/consistent_hashing/UUID2RingPartitionId.scala
+++ justin-db/src/main/scala/justin/db/consistent_hashing/UUID2RingPartitionId.scala
@@ -3,6 +3,6 @@ package justin.db.consistent_hashing
 import java.util.UUID
 import justin.db.consistent_hashing.Ring.RingPartitionId
 
-class UUID2RingPartitionId(ringSize: RingSize) extends (UUID => Ring.RingPartitionId) {
-  override def apply(id: UUID): RingPartitionId = scala.math.abs(id.hashCode()) % ringSize.size
+class UUID2RingPartitionId(ring: Ring) extends (UUID => Ring.RingPartitionId) {
+  override def apply(id: UUID): RingPartitionId = scala.math.abs(id.hashCode()) % ring.size
 }
