63c23e1f0eeec84356e93579546ae46d49d72598
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-14 14:52:52 +0100

    define returned type of "notContains" method of ClusterMembers object

--- justin-db/src/main/scala/justin/db/ClusterMembers.scala
+++ justin-db/src/main/scala/justin/db/ClusterMembers.scala
@@ -5,7 +5,7 @@ import justin.db.consistent_hashing.NodeId
 case class ClusterMembers(private val members: Map[NodeId, StorageNodeActorRef]) {
 
   def contains(nodeId: NodeId): Boolean = members.contains(nodeId)
-  def notContains(nodeId: NodeId) = !contains(nodeId)
+  def notContains(nodeId: NodeId): Boolean = !contains(nodeId)
 
   def add(nodeId: NodeId, ref: StorageNodeActorRef): ClusterMembers = {
     ClusterMembers(this.members + (nodeId -> ref))
