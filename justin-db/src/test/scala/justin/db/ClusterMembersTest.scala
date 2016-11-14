09255b25cf515dea70f6886f02f00a2f65e2c482
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-14 13:17:18 +0100

    test that "contains" method works

--- justin-db/src/test/scala/justin/db/ClusterMembersTest.scala
+++ justin-db/src/test/scala/justin/db/ClusterMembersTest.scala
@@ -32,4 +32,28 @@ class ClusterMembersTest extends FlatSpec with Matchers {
     updatedClusterMembers shouldBe ClusterMembers(Map(nodeId -> ref))
   }
 
+  it should "give false result when asking for non-existent element with \"contains\" method" in {
+    // given
+    val emptyClusterMembers = ClusterMembers.empty
+
+    // when
+    val exists = emptyClusterMembers.contains(NodeId(1))
+
+    // then
+    exists shouldBe false
+  }
+
+  it should "give positive result when asking for existent element with \"contains\" method" in {
+    // given
+    val nodeId = NodeId(100)
+    val ref = StorageNodeActorRef(ActorRef.noSender)
+    val emptyClusterMembers = ClusterMembers.empty.add(nodeId, ref)
+
+    // when
+    val exists = emptyClusterMembers.contains(NodeId(100))
+
+    // then
+    exists shouldBe true
+  }
+
 }
