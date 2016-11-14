7e5a2d78d1c754b940a7633f71c1d1d25da9af22
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-14 21:46:01 +0100

    rename WriteNodeActor to WriteRemoteNodeActor

--- justin-db/src/main/scala/justin/db/WriteRemoteNodeActor.scala
+++ justin-db/src/main/scala/justin/db/WriteRemoteNodeActor.scala
@@ -7,30 +7,30 @@ import justin.db.StorageNodeActor.PutReplicatedValue
 import justin.db.replication.W
 
 // TODO: assuming that W > 0
-class WriteNodeActor(w: W, storageNodeActors: List[StorageNodeActorRef]) extends Actor {
+class WriteRemoteNodeActor(w: W, storageNodeActors: List[StorageNodeActorRef]) extends Actor {
 
   val parentStorageNodeActor = context.parent
 
   var okWrites = 0
 
   override def receive: Receive = {
-    case WriteNodeActor.SuccessfulWrite if okWrites+1 == w.w =>
+    case WriteRemoteNodeActor.SuccessfulWrite if okWrites+1 == w.w =>
       parentStorageNodeActor ! StorageNodeActor.SuccessfulWrite
       context.stop(self)
-    case WriteNodeActor.SuccessfulWrite                      =>
+    case WriteRemoteNodeActor.SuccessfulWrite                      =>
       okWrites += 1
-    case WriteNodeActor.PropagateData(id, value)             =>
+    case WriteRemoteNodeActor.PropagateData(id, value)             =>
       storageNodeActors.foreach(_.storageNodeActor ! PutReplicatedValue(id, value))
   }
 }
 
-object WriteNodeActor {
+object WriteRemoteNodeActor {
 
   sealed trait WriteNodeMsg
   case class PropagateData(id: UUID, value: String) extends WriteNodeMsg
   case object SuccessfulWrite extends WriteNodeMsg
 
   def props(w: W, storageNodeActors: List[StorageNodeActorRef]): Props = {
-    Props(new WriteNodeActor(w, storageNodeActors))
+    Props(new WriteRemoteNodeActor(w, storageNodeActors))
   }
 }
