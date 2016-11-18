602b1423ddff085d1e8a1707f8be19157f804b1d
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-18 16:03:36 +0100

    read local data

---
+++ justin-db/src/main/scala/justin/db/LocalDataReadingService.scala
@@ -0,0 +1,19 @@
+package justin.db
+
+import java.util.UUID
+
+import justin.db.StorageNodeActor.StorageNodeReadingResult
+import justin.db.storage.PluggableStorage
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class LocalDataReadingService(storage: PluggableStorage)(implicit ec: ExecutionContext) {
+  def apply(id: UUID): Future[StorageNodeReadingResult] = {
+    storage.get(id.toString).map {
+      case Some(value) => StorageNodeReadingResult.Found(Data(id, value))
+      case None        => StorageNodeReadingResult.NotFound
+    }.recover {
+      case _           => StorageNodeReadingResult.FailedRead
+    }
+  }
+}
