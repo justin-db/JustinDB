effad53cf52ad0ee9896e01f68e5734f73a880aa
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-20 00:05:33 +0100

    test scenario of Local Data Saving Service: "successfully write data"

---
+++ justin-db/src/test/scala/justin/db/LocalDataSavingServiceTest.scala
@@ -0,0 +1,31 @@
+package justin.db
+
+import java.util.UUID
+
+import justin.db.StorageNodeActorProtocol.StorageNodeWritingResult
+import justin.db.storage.PluggableStorage
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class LocalDataSavingServiceTest extends FlatSpec with Matchers with ScalaFutures {
+
+  behavior of "Local Data Saving Service"
+
+  it should "successfully write data" in {
+    // given
+    val service = new LocalDataSavingService(new PluggableStorage {
+      override def get(key: String): Future[Option[String]] = ???
+      override def put(key: String, value: String): Future[Unit] = Future.successful(())
+    })
+    val data = Data(id = UUID.randomUUID(), value = "exemplary-value")
+
+    // when
+    val result = service.apply(data)
+
+    // then
+    whenReady(result) { _ == StorageNodeWritingResult.SuccessfulWrite }
+  }
+}
