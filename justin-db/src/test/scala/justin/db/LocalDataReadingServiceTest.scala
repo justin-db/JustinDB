2b3a55ac9492cdc28ebd9ffbe31bbe2140947641
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-19 13:58:47 +0100

    test scenario of Local Data Reading Service: "found data for existing key"

---
+++ justin-db/src/test/scala/justin/db/LocalDataReadingServiceTest.scala
@@ -0,0 +1,31 @@
+package justin.db
+
+import java.util.UUID
+
+import justin.db.StorageNodeActorProtocol.StorageNodeReadingResult
+import justin.db.storage.PluggableStorage
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class LocalDataReadingServiceTest extends FlatSpec with Matchers with ScalaFutures {
+
+  behavior of "Local Data Reading Service"
+
+  it should "found data for existing key" in {
+    // given
+    val service = new LocalDataReadingService(new PluggableStorage {
+      override def get(key: String): Future[Option[String]] = Future.successful(Option("value"))
+      override def put(key: String, value: String): Future[Unit] = ???
+    })
+    val id = UUID.randomUUID()
+
+    // when
+    val result = service.apply(id)
+
+    // then
+    whenReady(result) { _ == StorageNodeReadingResult.Found(Data(id, "value")) }
+  }
+}
