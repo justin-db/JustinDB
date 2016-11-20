35531a8e0941a9bda2901d41454ac97ac8f2b975
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-20 21:26:42 +0100

    test scenario of Remote Data Saving Service: "get info back that one of the saving is successful and second one has failed"

---
+++ justin-db/src/test/scala/justin/db/RemoteDataSavingServiceTest.scala
@@ -0,0 +1,40 @@
+package justin.db
+
+import java.util.UUID
+
+import akka.actor.{Actor, ActorSystem}
+import akka.testkit.{TestActorRef, TestKit}
+import justin.db.StorageNodeActorProtocol.{StorageNodeWriteData, StorageNodeWritingResult}
+import org.scalatest.{FlatSpecLike, Matchers}
+import org.scalatest.concurrent.ScalaFutures
+
+class RemoteDataSavingServiceTest extends TestKit(ActorSystem("test-system"))
+  with FlatSpecLike
+  with Matchers
+  with ScalaFutures {
+
+  behavior of "Remote Data Saving Service"
+
+  it should "get info back that one of the saving is successful and second one has failed" in {
+    // given
+    val service = new RemoteDataSavingService()(system.dispatcher)
+    val data = Data(id = UUID.randomUUID(), value = "exemplary-value")
+    val storageSuccessfulActorRef = testActorRef(msgBack = StorageNodeWritingResult.SuccessfulWrite)
+    val storageFailedActorRef     = testActorRef(msgBack = StorageNodeWritingResult.FailedWrite)
+    val storageNodeRefs           = List(storageSuccessfulActorRef, storageFailedActorRef).map(StorageNodeActorRef)
+
+    // when
+    val writingResult = service.apply(storageNodeRefs, data)
+
+    // then
+    whenReady(writingResult) { _ shouldBe List(StorageNodeWritingResult.SuccessfulWrite, StorageNodeWritingResult.FailedWrite) }
+  }
+
+  private def testActorRef(msgBack: => Any) = {
+    TestActorRef(new Actor {
+      override def receive: Receive = {
+        case StorageNodeWriteData.Local(id) => sender() ! msgBack
+      }
+    })
+  }
+}
