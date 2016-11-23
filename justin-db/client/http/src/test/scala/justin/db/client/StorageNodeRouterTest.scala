7f266c622c80f5e41692040d3a2131c69fa15699
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-23 11:09:13 +0100

    add storage node router's sceleton

---
+++ justin-db/client/http/src/test/scala/justin/db/client/StorageNodeRouterTest.scala
@@ -0,0 +1,10 @@
+package justin.db.client
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.{FlatSpec, Matchers}
+
+class StorageNodeRouterTest extends FlatSpec with Matchers with ScalatestRouteTest {
+
+  behavior of "Storage Node Router"
+
+}
