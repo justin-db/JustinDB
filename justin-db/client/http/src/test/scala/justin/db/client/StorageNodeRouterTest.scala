a3e98e9407d853267491fd0f3ae5f2e77638735b
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-23 11:47:44 +0100

    test StorageNodeRouter scenario: "get NoContent result for successfully written data"

--- justin-db/client/http/src/test/scala/justin/db/client/StorageNodeRouterTest.scala
+++ justin-db/client/http/src/test/scala/justin/db/client/StorageNodeRouterTest.scala
@@ -5,10 +5,13 @@ import java.util.UUID
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import justin.db.StorageNodeActorRef
-import justin.db.replication.R
+import justin.db.{Data, StorageNodeActorRef}
+import justin.db.client.StorageNodeRouter.PutValue
+import justin.db.client.StorageNodeRouter._
+import justin.db.replication.{R, W}
 import org.scalatest.{FlatSpec, Matchers}
 import spray.json._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
 import scala.concurrent.Future
 
@@ -16,6 +19,9 @@ class StorageNodeRouterTest extends FlatSpec with Matchers with ScalatestRouteTe
 
   behavior of "Storage Node Router"
 
+  /**
+    * GET part
+    */
   it should "get OK result for specific id and r" in {
     val value  = "value"
     val id     = UUID.randomUUID().toString
@@ -63,5 +69,21 @@ class StorageNodeRouterTest extends FlatSpec with Matchers with ScalatestRouteTe
 
   private def badRequest(error: String) = new HttpStorageNodeClient(StorageNodeActorRef(null)) {
     override def get(id: UUID, r: R): Future[GetValueResponse] = Future.successful(GetValueResponse.Failure(error))
+  }
+
+  /**
+    * PUT part
+    */
+  it should "get NoContent result for successfully written data" in {
+    val putValue = PutValue(id = UUID.randomUUID(), value = "value", w = 3)
+    val router   = new StorageNodeRouter(successfulWrite(putValue))
+
+    Post("/put", putValue) ~> Route.seal(router.routes) ~> check {
+      status shouldBe StatusCodes.NoContent
+    }
+  }
+
+  private def successfulWrite(putValue: PutValue) = new HttpStorageNodeClient(StorageNodeActorRef(null)) {
+    override def write(w: W, data: Data): Future[WriteValueResponse] = Future.successful(WriteValueResponse.Success)
   }
 }
