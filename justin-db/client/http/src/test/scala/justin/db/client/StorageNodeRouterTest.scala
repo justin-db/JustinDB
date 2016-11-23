8cfabdc24e84a2733acf0f8ce59ca5446d976d0d
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-23 11:33:54 +0100

    test StorageNodeRouter scenario: "get Failure result for specific id and r"

--- justin-db/client/http/src/test/scala/justin/db/client/StorageNodeRouterTest.scala
+++ justin-db/client/http/src/test/scala/justin/db/client/StorageNodeRouterTest.scala
@@ -40,11 +40,28 @@ class StorageNodeRouterTest extends FlatSpec with Matchers with ScalatestRouteTe
     }
   }
 
+  it should "get BadRequest result for specific id and r" in {
+    val value  = "value"
+    val id     = UUID.randomUUID().toString
+    val r      = 1
+    val error  = "Bad Request"
+    val router = new StorageNodeRouter(badRequest(error))
+
+    Get(s"/get?id=$id&r=$r") ~> Route.seal(router.routes) ~> check {
+      status                       shouldBe StatusCodes.BadRequest
+      responseAs[String].parseJson shouldBe JsObject("value" -> JsString(error))
+    }
+  }
+
   private def getFound(value: String) = new HttpStorageNodeClient(StorageNodeActorRef(null)) {
     override def get(id: UUID, r: R): Future[GetValueResponse] = Future.successful(GetValueResponse.Found(value))
   }
 
   private def notFound(value: String) = new HttpStorageNodeClient(StorageNodeActorRef(null)) {
     override def get(id: UUID, r: R): Future[GetValueResponse] = Future.successful(GetValueResponse.NotFound)
+  }
+
+  private def badRequest(error: String) = new HttpStorageNodeClient(StorageNodeActorRef(null)) {
+    override def get(id: UUID, r: R): Future[GetValueResponse] = Future.successful(GetValueResponse.Failure(error))
   }
 }
