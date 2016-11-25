f43420b49c26f86a46158b77e663e59498c897b8
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-25 18:18:55 +0100

    temp comment

--- justin-db/src/main/scala/justin/db/Data.scala
+++ justin-db/src/main/scala/justin/db/Data.scala
@@ -4,4 +4,4 @@ import java.util.UUID
 
 import justin.vector_clocks.VectorClock
 
-case class Data(id: UUID, value: String, vclock: VectorClock)
+case class Data(id: UUID, value: String /*, vclock: VectorClock*/)
