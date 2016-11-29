f504c306b9ac9194fc1c3e7e01c98b51a0334741
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-11-29 18:45:22 +0100

    refactor StorageNodeWriteService

--- justin-db/src/main/scala/justin/db/StorageNodeWriteService.scala
+++ justin-db/src/main/scala/justin/db/StorageNodeWriteService.scala
@@ -1,54 +1,42 @@
 package justin.db
 
-import java.util.UUID
-
 import justin.db.StorageNodeActorProtocol._
 import justin.consistent_hashing.{NodeId, Ring, UUID2RingPartitionId}
-import justin.db.replication.{N, PreferenceList}
+import justin.db.replication.{N, PreferenceList, W}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class StorageNodeWriteService(nodeId: NodeId, clusterMembers: ClusterMembers, ring: Ring, n: N,
-                              localDataSaver: LocalDataSavingService,
-                              remoteDataSaver: RemoteDataSavingService)(implicit ec: ExecutionContext)
+                              localDataWriter: LocalDataSavingService,
+                              remoteDataWriter: RemoteDataSavingService)(implicit ec: ExecutionContext)
   extends (StorageNodeWriteData => Future[StorageNodeWritingResult]) {
 
   override def apply(cmd: StorageNodeWriteData): Future[StorageNodeWritingResult] = cmd match {
-    case StorageNodeWriteData.Local(data)        => localDataSaver.apply(data)
+    case StorageNodeWriteData.Local(data)        => localDataWriter.apply(data)
     case StorageNodeWriteData.Replicate(w, data) =>
-      for {
-        preferenceList <- Future.successful(buildPreferenceList(data.id))
-        localTarget     = buildLocalTargetOpt(preferenceList)
-        remoteTargets   = buildRemoteTargets(preferenceList)
-        allWrites      <- writeToTargets(data, localTarget, remoteTargets)
-        okWrites        = allWrites.count(_ == StorageNodeWritingResult.SuccessfulWrite)
-      } yield {
-        okWrites >= w.w match {
-          case true  => StorageNodeWritingResult.SuccessfulWrite
-          case false => StorageNodeWritingResult.FailedWrite
-        }
-      }
+      val ringPartitionId = UUID2RingPartitionId.apply(data.id, ring)
+      val preferenceList  = PreferenceList(ringPartitionId, n, ring)
+      writeToTargets(data, preferenceList).map(sumUpWrites(w))
   }
 
-  private def buildPreferenceList(id: UUID) = {
-    val basePartitionId = UUID2RingPartitionId.apply(id, ring)
-    PreferenceList(basePartitionId, n, ring)
-  }
+  private def sumUpWrites(w: W)(writes: List[StorageNodeWritingResult]) = {
+    val okWrites = writes.count(_ == StorageNodeWritingResult.SuccessfulWrite)
 
-  private def buildLocalTargetOpt(preferenceList: List[NodeId]) = {
-    preferenceList.find(_ == nodeId)
-  }
-
-  private def buildRemoteTargets(preferenceList: List[NodeId]) = {
-    preferenceList.filterNot(_ == nodeId).distinct.flatMap(clusterMembers.get)
-  }
-
-  private def writeToTargets(data: Data, localTargetOpt: Option[NodeId], remoteTargets: List[StorageNodeActorRef]) = {
-    lazy val remoteSaves = remoteDataSaver.apply(remoteTargets, data)
-    lazy val localSave   = localDataSaver.apply(data)
-
-    localTargetOpt.fold(remoteSaves) { _ =>
-      localSave.zip(remoteSaves).map { case (lSaveResult, rSaveResults) => lSaveResult :: rSaveResults }
+    okWrites >= w.w match {
+      case true  => StorageNodeWritingResult.SuccessfulWrite
+      case false => StorageNodeWritingResult.FailedWrite
     }
   }
+
+  private def writeToTargets(data: Data, preferenceList: List[NodeId]) = {
+    val localTargetOpt = preferenceList.find(_ == nodeId)
+    val remoteTargets  = preferenceList.filterNot(_ == nodeId).distinct.flatMap(clusterMembers.get)
+
+    lazy val getRemoteWrites = remoteDataWriter.apply(remoteTargets, data)
+    lazy val getLocalWrite   = localDataWriter.apply(data)
+
+    localTargetOpt.fold(getRemoteWrites)(_ => getLocalWrite zip getRemoteWrites map converge )
+  }
+
+  private def converge(result: (StorageNodeWritingResult, List[StorageNodeWritingResult])) = result._1 :: result._2
 }
