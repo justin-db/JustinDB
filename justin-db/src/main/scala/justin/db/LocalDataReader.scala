96ea1b272df271b628af0ad5ffc176ab0dbd44e1
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-07 20:45:01 +0100

    handle ADT of PluggableStorage by Local Data Reader/Writer

--- justin-db/src/main/scala/justin/db/LocalDataReader.scala
+++ justin-db/src/main/scala/justin/db/LocalDataReader.scala
@@ -3,15 +3,16 @@ package justin.db
 import java.util.UUID
 
 import justin.db.StorageNodeActorProtocol._
-import justin.db.storage.PluggableStorage
+import justin.db.storage.{PluggableStorage, StorageGetData}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class LocalDataReader(storage: PluggableStorage)(implicit ec: ExecutionContext) {
   def apply(id: UUID): Future[StorageNodeReadingResult] = {
     storage.get(id).map {
-      case Some(data) => StorageNodeReadingResult.Found(data)
-      case None       => StorageNodeReadingResult.NotFound
+      case StorageGetData.Single(data)             => StorageNodeReadingResult.Found(data)
+      case StorageGetData.None                     => StorageNodeReadingResult.NotFound
+      case StorageGetData.Conflicted(data1, data2) => ??? // TODO
     }.recover {
       case _          => StorageNodeReadingResult.FailedRead
     }
--- justin-db/src/main/scala/justin/db/LocalDataWriter.scala
+++ justin-db/src/main/scala/justin/db/LocalDataWriter.scala
@@ -1,14 +1,22 @@
 package justin.db
 
 import justin.db.StorageNodeActorProtocol._
-import justin.db.storage.PluggableStorage
+import justin.db.storage.{PluggableStorage, StorageGetData}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class LocalDataWriter(storage: PluggableStorage)(implicit ec: ExecutionContext) {
   def apply(data: Data): Future[StorageNodeWritingResult] = {
-    storage.put(data)
-      .map(_ => StorageNodeWritingResult.SuccessfulWrite)
-      .recover { case _ => StorageNodeWritingResult.FailedWrite }
+    storage.get(data.id).flatMap {
+      case StorageGetData.None             => storage.put(data).map(_ => StorageNodeWritingResult.SuccessfulWrite)
+      case StorageGetData.Conflicted(_, _) => Future.successful(StorageNodeWritingResult.FailedWrite)
+      case StorageGetData.Single(existed)  =>
+        // jesli juz istnieje pod takim kluczem zapisana wartosc to sprawdzamy relacje zegarow wektorowych
+        // if data.VC <= saved.VC            - StorageNodeWritingResult.FailedWrite
+        // if data.VC conflict with saved.VC - StorageNodeWritingResult.ConflictedWrite
+        // if data.VC > saved.VC             - StorageNodeWritingResult.SuccessfulWrite
+
+        ???
+    } recover { case _ => StorageNodeWritingResult.FailedWrite }
   }
 }
