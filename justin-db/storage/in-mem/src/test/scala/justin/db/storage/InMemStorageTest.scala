edda42495a3130a0472110a802b0f734e4969ca7
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-09 12:13:44 +0100

    test In-memory storage scenario: "store conflicted data"

--- justin-db/storage/in-mem/src/test/scala/justin/db/storage/InMemStorageTest.scala
+++ justin-db/storage/in-mem/src/test/scala/justin/db/storage/InMemStorageTest.scala
@@ -10,6 +10,8 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import language.postfixOps
+
 class InMemStorageTest extends FlatSpec with Matchers {
 
   behavior of "In-memory storage"
@@ -17,6 +19,19 @@ class InMemStorageTest extends FlatSpec with Matchers {
   it should "store single data" in {
     // given
     val data = StoragePutData.Single(Data(id = UUID.randomUUID(), "some-data"))
+    val inMemStorage = new InMemStorage
+
+    // when
+    val result = Await.result(inMemStorage.put(data), atMost = 5 seconds)
+
+    // then
+    result shouldBe Ack
+  }
+
+  it should "store conflicted data" in {
+    // given
+    val id = UUID.randomUUID()
+    val data = StoragePutData.Conflict(id, Data(id, "some-data-1"), Data(id, "some-data-2"))
     val inMemStorage = new InMemStorage
 
     // when
