b61442c00148dd93ce6a6ad588ac87c650a1aa5c
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-09 17:16:07 +0100

    add test scenario for "Local Data Writer": "save successfully new data for not taken identificator"

--- justin-db/src/test/scala/justin/db/LocalDataWriterTest.scala
+++ justin-db/src/test/scala/justin/db/LocalDataWriterTest.scala
@@ -15,35 +15,19 @@ class LocalDataWriterTest extends FlatSpec with Matchers with ScalaFutures {
 
   behavior of "Local Data Writer"
 
-  it should "successfully write data" in {
-    pending
+  it should "save successfully new data for not taken identificator" in {
     // given
-    val service = new LocalDataWriter(new PluggableStorageProtocol {
-      override def get(id: UUID): Future[StorageGetData] = ???
+    val notTakenId = UUID.randomUUID()
+    val data = Data(notTakenId, "some-value")
+    val writer = new LocalDataWriter(new PluggableStorageProtocol {
+      override def get(id: UUID): Future[StorageGetData] = Future.successful(StorageGetData.None)
       override def put(cmd: StoragePutData): Future[Ack] = Ack.future
     })
-    val data = Data(id = UUID.randomUUID(), value = "exemplary-value")
 
     // when
-    val result = service.apply(data)
+    val result = writer.apply(data)
 
     // then
-    whenReady(result) { _ shouldBe StorageNodeWritingResult.SuccessfulWrite }
-  }
-
-  it should "recover for failed write" in {
-    pending
-    // given
-    val service = new LocalDataWriter(new PluggableStorageProtocol {
-      override def get(id: UUID): Future[StorageGetData] = ???
-      override def put(cmd: StoragePutData): Future[Ack] = Future.failed(new Exception)
-    })
-    val data = Data(id = UUID.randomUUID(), value = "exemplary-value")
-
-    // when
-    val result = service.apply(data)
-
-    // then
-    whenReady(result) { _ shouldBe StorageNodeWritingResult.FailedWrite }
+    whenReady(result) { _ shouldBe StorageNodeWritingResult.SuccessfulWrite}
   }
 }
