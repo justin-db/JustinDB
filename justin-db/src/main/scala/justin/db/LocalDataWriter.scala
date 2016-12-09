33e0709cdab2255be4aa8f25fabf2f8bbf2b4d3a
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-09 09:41:37 +0100

    resolve VectorClocks comparison when saving new data to already taken slot

--- justin-db/src/main/scala/justin/db/LocalDataWriter.scala
+++ justin-db/src/main/scala/justin/db/LocalDataWriter.scala
@@ -1,24 +1,33 @@
 package justin.db
 
+import justin.consistent_hashing.NodeId
 import justin.db.StorageNodeActorProtocol._
-import justin.db.storage.PluggableStorageProtocol.StorageGetData
+import justin.db.storage.PluggableStorageProtocol.{StorageGetData, StoragePutData}
 import justin.db.storage.PluggableStorageProtocol
+import justin.db.versioning.VectorClockComparator.VectorClockRelation
+import justin.db.versioning.{VCs2Compare, VectorClockComparator}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-// TODO:
 class LocalDataWriter(storage: PluggableStorageProtocol)(implicit ec: ExecutionContext) {
+
+  private val vectorClockComparator = new VectorClockComparator[NodeId]
+
   def apply(data: Data): Future[StorageNodeWritingResult] = {
     storage.get(data.id).flatMap {
-      case StorageGetData.None             => storage.put(data).map(_ => StorageNodeWritingResult.SuccessfulWrite)
-      case StorageGetData.Conflicted(_, _) => Future.successful(StorageNodeWritingResult.FailedWrite)
-      case StorageGetData.Single(existed)  =>
-        // jesli juz istnieje pod takim kluczem zapisana wartosc to sprawdzamy relacje zegarow wektorowych
-        // if data.VC <= saved.VC            - StorageNodeWritingResult.FailedWrite
-        // if data.VC conflict with saved.VC - StorageNodeWritingResult.ConflictedWrite
-        // if data.VC > saved.VC             - StorageNodeWritingResult.SuccessfulWrite
-
-        ???
+      case StorageGetData.None                           =>
+        storage.put(StoragePutData.Single(data)).map(_ => StorageNodeWritingResult.SuccessfulWrite)
+      case StorageGetData.Conflicted(existed1, existed2) =>
+        ??? // TODO: finish
+      case StorageGetData.Single(existed)                =>
+        vectorClockComparator.apply(VCs2Compare(existed.vclock, data.vclock)) match {
+          case VectorClockRelation.Predecessor           =>
+            Future.successful(StorageNodeWritingResult.FailedWrite)
+          case VectorClockRelation.Conflict              =>
+            storage.put(StoragePutData.Conflict(existed.id, existed, data)).map(_ => StorageNodeWritingResult.ConflictedWrite)
+          case VectorClockRelation.Consequent            =>
+            storage.put(StoragePutData.Single(data)).map(_ => StorageNodeWritingResult.SuccessfulWrite)
+        }
     } recover { case _ => StorageNodeWritingResult.FailedWrite }
   }
 }
