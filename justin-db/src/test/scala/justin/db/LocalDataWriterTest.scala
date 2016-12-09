cd7271b4fa0eea175557cd33d3aaa372929a1ef1
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-09 17:37:01 +0100

    add test scenario for "Local Data Writer": "fail to write predecessor to already stored data"

--- justin-db/src/test/scala/justin/db/LocalDataWriterTest.scala
+++ justin-db/src/test/scala/justin/db/LocalDataWriterTest.scala
@@ -2,9 +2,11 @@ package justin.db
 
 import java.util.UUID
 
+import justin.consistent_hashing.NodeId
 import justin.db.StorageNodeActorProtocol.StorageNodeWritingResult
 import justin.db.storage.PluggableStorageProtocol
 import justin.db.storage.PluggableStorageProtocol.{Ack, StorageGetData, StoragePutData}
+import justin.vector_clocks.{Counter, VectorClock}
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -42,6 +44,23 @@ class LocalDataWriterTest extends FlatSpec with Matchers with ScalaFutures {
 
     // when
     val result = writer.apply(data)
+
+    // then
+    whenReady(result) { _ shouldBe StorageNodeWritingResult.FailedWrite }
+  }
+
+  it should "fail to write predecessor to already stored data" in {
+    // given
+    val id = UUID.randomUUID()
+    val data    = Data(id, "some-value", VectorClock(Map(NodeId(1) -> Counter(2))))
+    val newData = Data(id, "some-value-2", VectorClock(Map(NodeId(1) -> Counter(1))))
+    val writer = new LocalDataWriter(new PluggableStorageProtocol {
+      override def get(id: UUID): Future[StorageGetData] = Future.successful(StorageGetData.Single(data))
+      override def put(cmd: StoragePutData): Future[Ack] = ???
+    })
+
+    // when
+    val result = writer.apply(newData)
 
     // then
     whenReady(result) { _ shouldBe StorageNodeWritingResult.FailedWrite }
