664fc29b7924c3f2c6bd0648ca7587ebebb976c5
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-09 12:18:22 +0100

    test In-memory storage scenario: "get single data for appropriate identificator"

--- justin-db/storage/in-mem/src/test/scala/justin/db/storage/InMemStorageTest.scala
+++ justin-db/storage/in-mem/src/test/scala/justin/db/storage/InMemStorageTest.scala
@@ -3,13 +3,12 @@ package justin.db.storage
 import java.util.UUID
 
 import justin.db.Data
-import justin.db.storage.PluggableStorageProtocol.{Ack, StoragePutData}
+import justin.db.storage.PluggableStorageProtocol.{Ack, StorageGetData, StoragePutData}
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import language.postfixOps
 
 class InMemStorageTest extends FlatSpec with Matchers {
@@ -39,5 +38,18 @@ class InMemStorageTest extends FlatSpec with Matchers {
 
     // then
     result shouldBe Ack
+  }
+
+  it should "get single data for appropriate identificator" in {
+    // given
+    val data = StoragePutData.Single(Data(id = UUID.randomUUID(), "some-data"))
+    val inMemStorage = new InMemStorage
+    Await.result(inMemStorage.put(data), atMost = 5 seconds)
+
+    // when
+    val result = Await.result(inMemStorage.get(data.data.id), atMost = 5 seconds)
+
+    // then
+    result shouldBe StorageGetData.Single(data.data)
   }
 }
