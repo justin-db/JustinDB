9500d71f2ced6f2d4cde628cf7c18cc423ecc847
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-11 09:20:09 +0100

    test creation of Vector Clock from plain string with numerical ids

--- justin-db/src/test/scala/justin/db/versioning/VectorClockOpsTest.scala
+++ justin-db/src/test/scala/justin/db/versioning/VectorClockOpsTest.scala
@@ -12,4 +12,11 @@ class VectorClockOpsTest extends FlatSpec with Matchers {
     "A:1, B:2".toVectorClock[String]      shouldBe VectorClock(Map("A" -> Counter(1), "B" -> Counter(2)))
     "A:1, B:1, C:1".toVectorClock[String] shouldBe VectorClock(Map("A" -> Counter(1), "B" -> Counter(1), "C" -> Counter(1)))
   }
+
+  it should "create Vector Clock instance from plain string with numerical ids" in {
+    import VectorClockOps.intAsId
+
+    ("1:2": VectorClock[Int])       shouldBe VectorClock(Map(1 -> Counter(2)))
+    ("1:2, 2:10": VectorClock[Int]) shouldBe VectorClock(Map(1 -> Counter(2), 2 -> Counter(10)))
+  }
 }
