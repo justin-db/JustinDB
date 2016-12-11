f59325a89c342e9f0df5a265df12bc5f66bd7bcd
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-11 09:29:23 +0100

    test creation of Vector Clock from plain string with id being type of NodeId

--- justin-db/src/test/scala/justin/db/versioning/VectorClockOpsTest.scala
+++ justin-db/src/test/scala/justin/db/versioning/VectorClockOpsTest.scala
@@ -1,5 +1,6 @@
 package justin.db.versioning
 
+import justin.consistent_hashing.NodeId
 import org.scalatest.{FlatSpec, Matchers}
 import justin.vector_clocks.{Counter, VectorClock}
 
@@ -18,5 +19,12 @@ class VectorClockOpsTest extends FlatSpec with Matchers {
 
     ("1:2": VectorClock[Int])       shouldBe VectorClock(Map(1 -> Counter(2)))
     ("1:2, 2:10": VectorClock[Int]) shouldBe VectorClock(Map(1 -> Counter(2), 2 -> Counter(10)))
+  }
+
+  it should "create Vector Clock instance from plain string with id being type of NodeId" in {
+    import VectorClockOps.nodeIdAsId
+
+    ("1:2": VectorClock[NodeId])       shouldBe VectorClock(Map(NodeId(1) -> Counter(2)))
+    ("1:2, 2:10": VectorClock[NodeId]) shouldBe VectorClock(Map(NodeId(1) -> Counter(2), NodeId(2) -> Counter(10)))
   }
 }
