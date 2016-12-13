07fc8050163fef90ab48ab09bdb84d241d38fd47
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-13 10:21:03 +0100

    define VectorClockHeader

---
+++ justin-db/client/http/src/main/scala/justin/db/client/VectorClockHeader.scala
@@ -0,0 +1,35 @@
+package justin.db.client
+
+import akka.http.scaladsl.model.headers.{ModeledCustomHeader, ModeledCustomHeaderCompanion}
+import justin.consistent_hashing.NodeId
+import justin.db.versioning.DataVersioning._
+import justin.db.versioning.NodeIdVectorClockBase64
+import justin.vector_clocks.VectorClock
+
+import scala.util.Try
+
+case class VectorClockHeaderException(msg: String) extends Exception(msg)
+
+case class VectorClockHeader(vectorClock: NodeIdVectorClock) extends ModeledCustomHeader[VectorClockHeader] {
+  override def companion: ModeledCustomHeaderCompanion[VectorClockHeader] = VectorClockHeader
+
+  override def value(): String = new NodeIdVectorClockBase64().encode(vectorClock) match {
+    case scala.util.Success(vclock) => vclock
+    case scala.util.Failure(_)      => throw VectorClockHeaderException("Couldn't encode vector clock of data")
+  }
+
+  override def renderInResponses(): Boolean = true
+  override def renderInRequests(): Boolean = true
+}
+
+object VectorClockHeader extends ModeledCustomHeaderCompanion[VectorClockHeader] {
+  override def name: String = "X-Vector-Clock"
+
+  override def parse(value: String): Try[VectorClockHeader] = {
+    new NodeIdVectorClockBase64()
+      .decode(value)
+      .map(VectorClockHeader(_))
+  }
+
+  def empty: VectorClockHeader = VectorClockHeader(VectorClock.apply[NodeId]())
+}
