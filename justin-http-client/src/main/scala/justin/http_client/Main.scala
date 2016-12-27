b1d38994f93ff1374af009e7bbf94ad7d8feca08
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-27 21:13:49 +0100

    start Http server on cluster member up registration

--- justin-http-client/src/main/scala/justin/http_client/Main.scala
+++ justin-http-client/src/main/scala/justin/http_client/Main.scala
@@ -1,6 +1,7 @@
 package justin.http_client
 
 import akka.actor.ActorSystem
+import akka.cluster.Cluster
 import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.RouteConcatenation
@@ -27,26 +28,32 @@ object Main extends App with RouteConcatenation {
   logger.info("Build Info: ")
   logger.info(BuildInfo.toString)
 
-  val storageNodeActorRef = StorageNodeActorRef {
-    val nodeId      = NodeId(config.getInt("node.id"))
-    val ring        = Ring(config.getInt("ring.cluster-nodes-size"), config.getInt("ring.creation-size"))
-    val storage     = new InMemStorage()
-    val replication = N(config.getInt("justin-db.replication.N"))
+  Cluster(system).registerOnMemberUp {
+    logger.info("Cluster is ready!")
 
-    system.actorOf(
-      props = StorageNodeActor.props(nodeId, storage, ring, replication),
-      name  = StorageNodeActor.name(nodeId)
-    )
+    // STORAGE ACTOR
+    val storageNodeActorRef = StorageNodeActorRef {
+      val nodeId      = NodeId(config.getInt("node.id"))
+      val ring        = Ring(config.getInt("ring.cluster-nodes-size"), config.getInt("ring.creation-size"))
+      val storage     = new InMemStorage()
+      val replication = N(config.getInt("justin-db.replication.N"))
+
+      system.actorOf(
+        props = StorageNodeActor.props(nodeId, storage, ring, replication),
+        name  = StorageNodeActor.name(nodeId)
+      )
+    }
+
+    // HTTP API
+    val routes = logRequestResult(system.name) {
+      new HttpRouter(new ActorRefStorageNodeClient(storageNodeActorRef)).routes ~
+        new HealthCheckRouter().routes ~
+        new BuildInfoRouter().routes
+    }
+
+    Http()
+      .bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+      .map { binding => logger.info(s"HTTP server started at ${binding.localAddress}") }
+      .recover { case ex => logger.error(ex, "Could not start HTTP server") }
   }
-
-  val routes = logRequestResult(system.name) {
-    new HttpRouter(new ActorRefStorageNodeClient(storageNodeActorRef)).routes ~
-      new HealthCheckRouter().routes ~
-      new BuildInfoRouter().routes
-  }
-
-  Http()
-    .bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
-    .map { binding => logger.info(s"HTTP server started at ${binding.localAddress}") }
-    .recover { case ex => logger.error(ex, "Could not start HTTP server") }
 }
