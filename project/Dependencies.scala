dd4805f3f614975a6f440b1b67c01161633b5cf7
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-28 07:22:19 +0100

    bump akka 2.4.14 -> 2.4.16 and akka-http 10.0.0 -> 10.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Version {
   val scala       = "2.12.1"
   val scalaBinary = scala.substring(0,4)
 
-  val akka        = "2.4.14"
-  val akkaHttp    = "10.0.0"
+  val akka        = "2.4.16"
+  val akkaHttp    = "10.0.1"
   val scalatest   = "3.0.1"
   val scalacheck  = "1.13.4"
   val sigarLoader = "1.6.6"
