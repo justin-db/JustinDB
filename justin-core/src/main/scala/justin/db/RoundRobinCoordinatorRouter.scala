ddb107fbcd69b735d1e16d263f07cebd7cf19056
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-29 21:15:33 +0100

    refactor RoundRobinCoordinatorRouter

--- justin-core/src/main/scala/justin/db/RoundRobinCoordinatorRouter.scala
+++ justin-core/src/main/scala/justin/db/RoundRobinCoordinatorRouter.scala
@@ -11,11 +11,15 @@ import scala.concurrent.ExecutionContext
 object RoundRobinCoordinatorRouter {
   def routerName: String = "CoordinatorRouter"
 
+  private val pool = RoundRobinPool(
+    nrOfInstances = 5,
+    resizer = Some(DefaultResizer(lowerBound = 2, upperBound = 15))
+  )
+
   def props(nodeId: NodeId, ring: Ring, n: N, storage: PluggableStorageProtocol)(implicit ec: ExecutionContext): Props = {
     val readCoordinator  = new ReplicaReadCoordinator(nodeId, ring, n, new ReplicaLocalReader(storage), new ReplicaRemoteReader)
     val writeCoordinator = new ReplicaWriteCoordinator(nodeId, ring, n, new ReplicaLocalWriter(storage), new ReplicaRemoteWriter)
-    val actorProps = StorageNodeWorkerActor.props(readCoordinator, writeCoordinator)
 
-    RoundRobinPool(nrOfInstances = 5, resizer = Some(DefaultResizer(lowerBound = 2, upperBound = 15))).props(actorProps)
+    pool.props(StorageNodeWorkerActor.props(readCoordinator, writeCoordinator))
   }
 }
