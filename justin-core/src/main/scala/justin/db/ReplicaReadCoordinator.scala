087d760d2294ab44fca47f590c486bfd0c904c61
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-31 16:01:59 +0100

    refactor ReplicaReadCoordinator

--- justin-core/src/main/scala/justin/db/ReplicaReadCoordinator.scala
+++ justin-core/src/main/scala/justin/db/ReplicaReadCoordinator.scala
@@ -1,8 +1,10 @@
 package justin.db
 
+import java.util.UUID
+
 import justin.consistent_hashing.{NodeId, Ring, UUID2RingPartitionId}
 import justin.db.StorageNodeActorProtocol._
-import justin.db.replication.{N, PreferenceList}
+import justin.db.replication.{N, PreferenceList, R}
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -13,17 +15,22 @@ class ReplicaReadCoordinator(
 )(implicit ec: ExecutionContext) extends ((StorageNodeReadData, ClusterMembers) => Future[StorageNodeReadingResult]) {
 
   override def apply(cmd: StorageNodeReadData, clusterMembers: ClusterMembers): Future[StorageNodeReadingResult] = cmd match {
-    case StorageNodeReadData.Local(id)         => localDataReader.apply(id)
-    case StorageNodeReadData.Replicated(r, id) =>
-      val ringPartitionId = UUID2RingPartitionId.apply(id, ring)
-      val preferenceList  = PreferenceList(ringPartitionId, n, ring)
+    case StorageNodeReadData.Local(id)         => coordinateLocal(id)
+    case StorageNodeReadData.Replicated(r, id) => coordinateReplicated(r, id, clusterMembers)
+  }
 
-      ResolveNodeTargets(nodeId, preferenceList, clusterMembers) match {
-        case ResolvedTargets(true, remotes)  if remotes.size + 1 >= r.r =>
-          (localDataReader.apply(id) zip remoteDataReader.apply(remotes, id)).map(converge).map(ReachConsensusReplicatedReads(r))
-        case ResolvedTargets(false, remotes) if remotes.size     >= r.r =>
-          remoteDataReader.apply(remotes, id).map(ReachConsensusReplicatedReads(r))
-        case _ => Future.successful(StorageNodeReadingResult.FailedRead)
-      }
+  private def coordinateLocal(id: UUID) = localDataReader.apply(id)
+
+  private def coordinateReplicated(r: R, id: UUID, clusterMembers: ClusterMembers) = {
+    val ringPartitionId = UUID2RingPartitionId.apply(id, ring)
+    val preferenceList  = PreferenceList(ringPartitionId, n, ring)
+
+    ResolveNodeTargets(nodeId, preferenceList, clusterMembers) match {
+      case ResolvedTargets(true, remotes)  if remotes.size + 1 >= r.r =>
+        (localDataReader.apply(id) zip remoteDataReader.apply(remotes, id)).map(converge).map(ReachConsensusReplicatedReads(r))
+      case ResolvedTargets(false, remotes) if remotes.size     >= r.r =>
+        remoteDataReader.apply(remotes, id).map(ReachConsensusReplicatedReads(r))
+      case _ => Future.successful(StorageNodeReadingResult.FailedRead)
+    }
   }
 }
