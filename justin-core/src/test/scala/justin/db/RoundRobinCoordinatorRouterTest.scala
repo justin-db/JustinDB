518af431c62420469d66e41337ea6aebf51d1ecd
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-31 16:39:12 +0100

    test Round-Robin coordinator Router name

---
+++ justin-core/src/test/scala/justin/db/RoundRobinCoordinatorRouterTest.scala
@@ -0,0 +1,12 @@
+package justin.db
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class RoundRobinCoordinatorRouterTest extends FlatSpec with Matchers {
+
+  behavior of "Round-Robin coordinator Router"
+
+  it should "get router name" in {
+    RoundRobinCoordinatorRouter.routerName shouldBe "CoordinatorRouter"
+  }
+}
