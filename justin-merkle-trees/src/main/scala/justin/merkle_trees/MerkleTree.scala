75e61ca3579a7e6f9e777f359aa45986022966cb
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-01-04 12:50:39 +0100

    move private method to unapply as inner one

--- justin-merkle-trees/src/main/scala/justin/merkle_trees/MerkleTree.scala
+++ justin-merkle-trees/src/main/scala/justin/merkle_trees/MerkleTree.scala
@@ -13,28 +13,29 @@ object MerkleTree {
   def unapply(blocks: Seq[Block])(implicit ev: MerkleDigest[Block]): Option[MerkleTree] = unapply(blocks.toArray)
 
   def unapply(blocks: Array[Block])(implicit ev: MerkleDigest[Block]): Option[MerkleTree] = {
+
+    def blockToLeaf(b: Block)(implicit ev: MerkleDigest[Block]) = MerkleLeaf(ev.digest(b))
+
+    def buildTree(leafs: Seq[MerkleLeaf])(implicit ev: MerkleDigest[Block]) = Try {
+      var trees: Seq[MerkleTree] = leafs
+
+      while (trees.length > 1) {
+        trees = trees.grouped(2)
+          .map(x => mergeTrees(x(0), x(1)))
+          .toSeq
+      }
+      trees.head
+    }
+
+    def mergeTrees(n1: MerkleTree, n2: MerkleTree)(implicit ev: MerkleDigest[Block]) = {
+      val mergedDigest = n1.digest + n2.digest
+      val hash = ev.digest(mergedDigest.hash)
+      MerkleHashNode(hash, n1, n2)
+    }
+
     val allBlocks = blocks ++ zeroed(blocks)
     val leafs = allBlocks.map(blockToLeaf)
     buildTree(leafs).toOption
-  }
-
-  private def blockToLeaf(b: Block)(implicit ev: MerkleDigest[Block]) = MerkleLeaf(ev.digest(b))
-
-  private def buildTree(leafs: Seq[MerkleLeaf])(implicit ev: MerkleDigest[Block]) = Try {
-    var trees: Seq[MerkleTree] = leafs
-
-    while (trees.length > 1) {
-      trees = trees.grouped(2)
-        .map(x => mergeTrees(x(0), x(1)))
-        .toSeq
-    }
-    trees.head
-  }
-
-  private def mergeTrees(n1: MerkleTree, n2: MerkleTree)(implicit ev: MerkleDigest[Block]) = {
-    val mergedDigest = n1.digest + n2.digest
-    val hash = ev.digest(mergedDigest.hash)
-    MerkleHashNode(hash, n1, n2)
   }
 
   def zeroed(blocks: Seq[Block]): Array[Array[Byte]] = {
