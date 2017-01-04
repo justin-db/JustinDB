5ed5e2486063ddb9f0a03fd3e69ce225e9d0a509
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-01-04 15:34:39 +0100

    add indexing of Merkle Tree nodes

--- justin-merkle-trees/src/main/scala/justin/merkle_trees/MerkleTree.scala
+++ justin-merkle-trees/src/main/scala/justin/merkle_trees/MerkleTree.scala
@@ -3,23 +3,26 @@ package justin.merkle_trees
 import scala.util.Try
 
 sealed trait MerkleTree {
+  def nodeId: NodeId
   def digest: Digest
 }
-case class MerkleHashNode(digest: Digest, left: MerkleTree, right: MerkleTree) extends MerkleTree
-case class MerkleLeaf(digest: Digest) extends MerkleTree
+case class MerkleHashNode(nodeId: NodeId, digest: Digest, left: MerkleTree, right: MerkleTree) extends MerkleTree
+case class MerkleLeaf(nodeId: NodeId, digest: Digest) extends MerkleTree
 
 object MerkleTree {
 
   def unapply(blocks: Seq[Block])(implicit ev: MerkleDigest[Block]): Option[MerkleTree] = unapply(blocks.toArray)
 
   def unapply(blocks: Array[Block])(implicit ev: MerkleDigest[Block]): Option[MerkleTree] = {
+    sealed trait TempMerkleTree { def digest: Digest }
+    case class TempMerkleHashNode(digest: Digest, left: TempMerkleTree, right: TempMerkleTree) extends TempMerkleTree
+    case class TempMerkleLeaf(digest: Digest) extends TempMerkleTree
 
-    def blockToLeaf(b: Block)(implicit ev: MerkleDigest[Block]) = MerkleLeaf(ev.digest(b))
+    def blockToLeaf(b: Block)(implicit ev: MerkleDigest[Block]) = TempMerkleLeaf(ev.digest(b))
 
     def buildTree(blocks: Array[Block])(implicit ev: MerkleDigest[Block]) = Try {
       val leafs = blocks.map(blockToLeaf)
-
-      var trees: Seq[MerkleTree] = leafs
+      var trees: Seq[TempMerkleTree] = leafs
 
       while (trees.length > 1) {
         trees = trees.grouped(2)
@@ -29,13 +32,28 @@ object MerkleTree {
       trees.head
     }
 
-    def mergeTrees(n1: MerkleTree, n2: MerkleTree)(implicit ev: MerkleDigest[Block]) = {
+    def mergeTrees(n1: TempMerkleTree, n2: TempMerkleTree)(implicit ev: MerkleDigest[Block]) = {
       val mergedDigest = n1.digest + n2.digest
       val hash = ev.digest(mergedDigest.hash)
-      MerkleHashNode(hash, n1, n2)
+      TempMerkleHashNode(hash, n1, n2)
     }
 
-    buildTree(blocks ++ zeroed(blocks)).toOption
+    def toFinalForm(tmt: TempMerkleTree): MerkleTree = {
+      var counter = -1
+
+      def toMerkle(mt: TempMerkleTree): MerkleTree = {
+        counter += 1
+        mt match {
+          case TempMerkleHashNode(digest, left, right) => MerkleHashNode(NodeId(counter), digest, toMerkle(left), toMerkle(right))
+          case TempMerkleLeaf(digest)                  => MerkleLeaf(NodeId(counter), digest)
+        }
+      }
+      toMerkle(tmt)
+    }
+
+    buildTree(blocks ++ zeroed(blocks))
+      .toOption
+      .map(toFinalForm)
   }
 
   def zeroed(blocks: Seq[Block]): Array[Array[Byte]] = {
