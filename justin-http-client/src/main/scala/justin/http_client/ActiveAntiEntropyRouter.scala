1a5d3b885049fca3ca59994381058d14978cf7ed
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-01-09 10:06:17 +0100

    pass Active-Anti-Entropy actor instance to its companion router

--- justin-http-client/src/main/scala/justin/http_client/ActiveAntiEntropyRouter.scala
+++ justin-http-client/src/main/scala/justin/http_client/ActiveAntiEntropyRouter.scala
@@ -4,10 +4,11 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 import akka.stream.Materializer
+import justin.db.entropy.ActiveAntiEntropyActorRef
 
 import scala.concurrent.ExecutionContext
 
-class ActiveAntiEntropyRouter(implicit ec: ExecutionContext, mat: Materializer) {
+class ActiveAntiEntropyRouter(actorRef: ActiveAntiEntropyActorRef)(implicit ec: ExecutionContext, mat: Materializer) {
 
   val routes: Route = path("aae" / "run") {
     post { complete(StatusCodes.NoContent) }
--- justin-http-client/src/test/scala/justin/http_client/ActiveAntiEntropyRouterTest.scala
+++ justin-http-client/src/test/scala/justin/http_client/ActiveAntiEntropyRouterTest.scala
@@ -3,6 +3,7 @@ package justin.http_client
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import justin.db.entropy.ActiveAntiEntropyActorRef
 import org.scalatest.{FlatSpec, Matchers}
 
 class ActiveAntiEntropyRouterTest extends FlatSpec with Matchers with ScalatestRouteTest {
@@ -10,7 +11,7 @@ class ActiveAntiEntropyRouterTest extends FlatSpec with Matchers with ScalatestR
   behavior of "Active-Anti-Entropy Router"
 
   it should "post run active-anti-entropy mechanism" in {
-    Post("/aae/run") ~> Route.seal(new ActiveAntiEntropyRouter().routes) ~> check {
+    Post("/aae/run") ~> Route.seal(new ActiveAntiEntropyRouter(ActiveAntiEntropyActorRef(null)).routes) ~> check {
       status shouldBe StatusCodes.NoContent
     }
   }
