589ea6e62db5878218d9fae94488c0b6b213ce92
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-01-10 08:37:23 +0100

    converge tuple test

---
+++ justin-core/src/test/scala/justin/db/versioning/ConvergeTest.scala
@@ -0,0 +1,19 @@
+package justin.db.versioning
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class ConvergeTest extends FlatSpec with Matchers {
+
+  behavior of "converge"
+
+  it should "converge tuple" in {
+    // given
+    val tuple = (1, List(2,3,4,5))
+
+    // when
+    val result = justin.db.converge(tuple)
+
+    // then
+    result shouldBe List(1,2,3,4,5)
+  }
+}
