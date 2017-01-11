a3c5a94aab8b10c89fd880e41219b472d2149aff
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-01-11 18:53:57 +0100

    keep primaries and replicas in divergent structures

--- justin-storage-in-mem/src/main/scala/justin/db/storage/InMemStorage.scala
+++ justin-storage-in-mem/src/main/scala/justin/db/storage/InMemStorage.scala
@@ -2,6 +2,7 @@ package justin.db.storage
 
 import java.util.UUID
 
+import justin.consistent_hashing.Ring.RingPartitionId
 import justin.db.Data
 import justin.db.storage.PluggableStorageProtocol.{Ack, DataOriginality, StorageGetData, StoragePutData}
 
@@ -15,22 +16,27 @@ class InMemStorage(implicit ec: ExecutionContext) extends PluggableStorageProtoc
 
   private case class MapVal(data1: Data, data2: Option[Data])
 
-  private var values = mutable.Map.empty[UUID, MapVal]
+  private type MMap           = mutable.Map[RingPartitionId, Map[UUID, MapVal]]
+  private var primaries: MMap = mutable.Map.empty[RingPartitionId, Map[UUID, MapVal]]
+  private var replicas: MMap  = mutable.Map.empty[RingPartitionId, Map[UUID, MapVal]]
 
-  // TODO: handle resolving of originality of Data
-  override def get(id: UUID)(resolveOriginality: (UUID) => DataOriginality): Future[StorageGetData] = Future.successful  {
-    values.get(id) match {
-      case None                             => StorageGetData.None
-      case Some(MapVal(data1, Some(data2))) => StorageGetData.Conflicted(data1, data2)
-      case Some(MapVal(data1, None))        => StorageGetData.Single(data1)
+  override def get(id: UUID)(resolveOriginality: (UUID) => DataOriginality): Future[StorageGetData] = Future.successful {
+    def get(mmap: MMap, partitionId: RingPartitionId, id: UUID) = {
+      mmap.get(partitionId).fold[StorageGetData](StorageGetData.None) { _.get(id) match {
+        case None                             => StorageGetData.None
+        case Some(MapVal(data1, Some(data2))) => StorageGetData.Conflicted(data1, data2)
+        case Some(MapVal(data1, None))        => StorageGetData.Single(data1)
+      }}
+    }
+
+    resolveOriginality(id) match {
+      case DataOriginality.Primary(partitionId) => get(primaries, partitionId, id)
+      case DataOriginality.Replica(partitionId) => get(replicas, partitionId, id)
     }
   }
 
+  // TODO: handle resolving of originality of Data
   override def put(cmd: StoragePutData): Future[Ack] = {
-    values = cmd match {
-      case StoragePutData.Single(data)               => values + (data.id -> MapVal(data, None))
-      case StoragePutData.Conflict(id, data1, data2) => values + (id -> MapVal(data1, Option(data2)))
-    }
     Ack.future
   }
 }
