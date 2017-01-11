24b98aab21661d8ae9b73945c69e7e11151500b8
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-01-11 16:12:36 +0100

    test resolver of data originality

--- justin-core/src/main/scala/justin/db/ResolveDataOriginality.scala
+++ justin-core/src/main/scala/justin/db/ResolveDataOriginality.scala
@@ -5,9 +5,6 @@ import java.util.UUID
 import justin.consistent_hashing.{NodeId, Ring, UUID2RingPartitionId}
 import justin.db.storage.PluggableStorageProtocol.DataOriginality
 
-/**
-  * Created by new on 11.01.2017.
-  */
 class ResolveDataOriginality(nodeId: NodeId, ring: Ring) extends (UUID => DataOriginality) {
   override def apply(id: UUID): DataOriginality = {
     val partitionId = UUID2RingPartitionId(id, ring)
---
+++ justin-core/src/test/scala/justin/db/ResolveDataOriginalityTest.scala
@@ -0,0 +1,40 @@
+package justin.db
+
+import java.util.UUID
+
+import justin.consistent_hashing.{NodeId, Ring}
+import justin.db.storage.PluggableStorageProtocol.DataOriginality
+import org.scalatest.{FlatSpec, Matchers}
+
+class ResolveDataOriginalityTest extends FlatSpec with Matchers {
+
+  behavior of "Data Originality Resolver"
+
+  it should "reason exemplary data's id as a replica" in {
+    // given
+    val nodeId   = NodeId(0)
+    val ring     = Ring.apply(nodesSize = 3, partitionsSize = 21)
+    val resolver = new ResolveDataOriginality(nodeId, ring)
+    val id       = UUID.fromString("179d6eb0-681d-4277-9caf-3d6d60e9faf9")
+
+    // when
+    val originality = resolver.apply(id)
+
+    // then
+    originality shouldBe a[DataOriginality.Replica]
+  }
+
+  it should "reason exemplary data's id as a primary" in {
+    // given
+    val nodeId   = NodeId(0)
+    val ring     = Ring.apply(nodesSize = 3, partitionsSize = 21)
+    val resolver = new ResolveDataOriginality(nodeId, ring)
+    val id       = UUID.fromString("16ec44cd-5b4e-4b38-a647-206c1dc11b50")
+
+    // when
+    val originality = resolver.apply(id)
+
+    // then
+    originality shouldBe a[DataOriginality.Primary]
+  }
+}
