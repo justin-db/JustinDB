9471c6f3942606deada23f7e3ab8a833e07c9b16
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-02-04 19:30:56 +0100

    reorganize ResolveNodeTargets code

--- justin-core/src/main/scala/justin/db/ResolveNodeTargets.scala
+++ justin-core/src/main/scala/justin/db/ResolveNodeTargets.scala
@@ -9,8 +9,8 @@ object ResolveNodeTargets {
 
   def apply(nodeId: NodeId, preferenceList: PreferenceList, clusterMembers: ClusterMembers): ResolvedTargets = {
     ResolvedTargets(
-      local   = preferenceList.primaryNodeId == nodeId,
-      remotes = preferenceList.replicasNodeId.flatMap(clusterMembers.get)
+      local   = preferenceList.all.contains(nodeId),
+      remotes = preferenceList.all.flatMap(clusterMembers.get)
     )
   }
 }
