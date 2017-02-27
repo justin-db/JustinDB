653ba186288ba83a11e7025186d38a37828a5f36
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-02-27 22:41:12 +0100

    hide reading values from config behind trait

--- justin-http-client/src/main/scala/justin/http_client/Main.scala
+++ justin-http-client/src/main/scala/justin/http_client/Main.scala
@@ -6,7 +6,7 @@ import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 import justin.consistent_hashing.{NodeId, Ring}
 import justin.db.client.ActorRefStorageNodeClient
 import justin.db.entropy.{ActiveAntiEntropyActor, ActiveAntiEntropyActorRef}
@@ -14,8 +14,10 @@ import justin.db.replication.N
 import justin.db.storage.InMemStorage
 import justin.db.{StorageNodeActor, StorageNodeActorRef}
 
-object Main extends App {
-  val config = ConfigFactory.parseString(s"akka.cluster.roles = [${StorageNodeActor.role}]")
+object Main extends App with ServiceConfig {
+
+  override protected val config: Config = ConfigFactory
+    .parseString(s"akka.cluster.roles = [${StorageNodeActor.role}]")
     .withFallback(ConfigFactory.load())
 
   implicit val system       = ActorSystem("justin-db-cluster-system", config)
@@ -24,7 +26,7 @@ object Main extends App {
 
   val logger = Logging(system, getClass)
 
-  logger.info("JustinDB " + BuildInfo.version)
+  logger.info("JustinDB version: " + BuildInfo.version)
   logger.info("Build Info: ")
   logger.info(BuildInfo.toString)
 
@@ -33,10 +35,10 @@ object Main extends App {
 
     // STORAGE ACTOR
     val storageNodeActorRef = new ActorRefStorageNodeClient(StorageNodeActorRef {
-      val nodeId      = NodeId(config.getInt("node.id"))
-      val ring        = Ring(config.getInt("ring.cluster-nodes-size"), config.getInt("ring.creation-size"))
+      val nodeId      = NodeId(`nodes-id`)
+      val ring        = Ring(`ring-cluster-size`, `ring-partitions`)
       val storage     = new InMemStorage()
-      val replication = N(config.getInt("justin-db.replication.N"))
+      val replication = N(`replication-n`)
 
       system.actorOf(
         props = StorageNodeActor.props(nodeId, storage, ring, replication),
@@ -56,7 +58,7 @@ object Main extends App {
     }
 
     Http()
-      .bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+      .bindAndHandle(routes, `http-interface`, `http-port`)
       .map { binding => logger.info(s"HTTP server started at ${binding.localAddress}") }
       .recover { case ex => logger.error(ex, "Could not start HTTP server") }
   }
---
+++ justin-http-client/src/main/scala/justin/http_client/ServiceConfig.scala
@@ -0,0 +1,15 @@
+package justin.http_client
+
+import com.typesafe.config.Config
+
+trait ServiceConfig {
+  protected def config: Config
+
+  lazy val `nodes-id`: Int          = config.getInt("node.id")
+  lazy val `ring-cluster-size`: Int = config.getInt("ring.cluster-size")
+  lazy val `ring-partitions`: Int   = config.getInt("ring.partitions")
+  lazy val `replication-n`: Int     = config.getInt("justin-db.replication.N")
+
+  lazy val `http-interface`: String = config.getString("http.interface")
+  lazy val `http-port`: Int         = config.getInt("http.port")
+}
