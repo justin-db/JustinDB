49a975fee2aa898e2ed1c70f51b04b407fc153e4
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-03-23 19:07:11 +0000

    import implicitConversions language feature

--- justin-core/src/main/scala/justin/db/Data.scala
+++ justin-core/src/main/scala/justin/db/Data.scala
@@ -6,6 +6,7 @@ import justin.db.replication.PreferenceList
 import justin.db.storage.JustinData
 import justin.db.versioning.{NodeIdVectorClock, NodeIdVectorClockBase64}
 import justin.vector_clocks.VectorClock
+import scala.language.implicitConversions
 
 case class Data(id: UUID, value: String, vclock: NodeIdVectorClock = VectorClock(), timestamp: Long = System.currentTimeMillis())
 
@@ -16,9 +17,13 @@ object Data {
     data.copy(vclock = nodeIds.foldLeft(data.vclock)(_ increase _))
   }
 
-  // TODO: check if encodeding of VClock is possible (make it typesafe)
-  implicit def toInternal(data: Data): JustinData = JustinData(data.id, data.value, new NodeIdVectorClockBase64().encode(data.vclock).get, data.timestamp)
+  implicit def toInternal(data: Data): JustinData = {
+    val encodedVClock = new NodeIdVectorClockBase64().encode(data.vclock).get // TODO: check if encodeding of VClock is possible (make it typesafe)
+    JustinData(data.id, data.value, encodedVClock, data.timestamp)
+  }
 
-  // TODO: check if decodeing of VClock is possible (make it typesafe)
-  implicit def fromInternal(justinData: JustinData): Data = Data(justinData.id, justinData.value, new NodeIdVectorClockBase64().decode(justinData.vclock).get, justinData.timestamp)
+  implicit def fromInternal(justinData: JustinData): Data = {
+    val decodedVClock = new NodeIdVectorClockBase64().decode(justinData.vclock).get // TODO: check if decodeing of VClock is possible (make it typesafe)
+    Data(justinData.id, justinData.value, decodedVClock, justinData.timestamp)
+  }
 }
