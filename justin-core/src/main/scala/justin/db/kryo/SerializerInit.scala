cabd474ff1796daf76125a0a6fdce604b9e92b59
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-03-27 20:57:58 +0200

    register kryo serializer in conf

---
+++ justin-core/src/main/scala/justin/db/kryo/SerializerInit.scala
@@ -0,0 +1,10 @@
+package justin.db.kryo
+
+import com.esotericsoftware.kryo.Kryo
+import com.typesafe.scalalogging.StrictLogging
+
+class SerializerInit extends StrictLogging {
+  logger.warn("Initialized Kryo")
+
+
+}
