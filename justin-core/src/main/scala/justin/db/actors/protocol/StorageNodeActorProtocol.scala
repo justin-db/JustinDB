a22fa59bce4a393fc7e805c89e62d95bf60fd896
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-03-30 21:03:43 +0200

    delete comments

--- justin-core/src/main/scala/justin/db/actors/protocol/StorageNodeActorProtocol.scala
+++ justin-core/src/main/scala/justin/db/actors/protocol/StorageNodeActorProtocol.scala
@@ -5,14 +5,11 @@ import java.util.UUID
 import justin.db.Data
 import justin.db.replica.{R, W}
 
-// ----- READ PART ----
-// INPUT
 sealed trait StorageNodeReadData
 object StorageNodeReadData {
   case class Local(id: UUID)            extends StorageNodeReadData
   case class Replicated(r: R, id: UUID) extends StorageNodeReadData
 }
-// OUTPUT
 sealed trait StorageNodeReadingResult
 object StorageNodeReadingResult {
   case class Found(data: Data)           extends StorageNodeReadingResult
@@ -20,20 +17,16 @@ object StorageNodeReadingResult {
   case object NotFound                   extends StorageNodeReadingResult
   case object FailedRead                 extends StorageNodeReadingResult
 }
-// ------
 
-// ----- WRITE PART ----
-// INPUT
 sealed trait StorageNodeWriteData
 case class StorageNodeWriteDataLocal(data: Data) extends StorageNodeWriteData
 object StorageNodeWriteData {
   case class Replicate(w: W, data: Data) extends StorageNodeWriteData
 }
-// OUTPUT
+
 sealed trait StorageNodeWritingResult
 case class StorageNodeSuccessfulWrite(id: UUID)            extends StorageNodeWritingResult
 object StorageNodeWritingResult {
   case object FailedWrite                                  extends StorageNodeWritingResult
   case class ConflictedWrite(oldData: Data, newData: Data) extends StorageNodeWritingResult
 }
-// ------
