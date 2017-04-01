c5c8589002b124d9522df5705c3bb8caafd5ee17
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-04-01 16:20:09 +0200

    test of Data serializer

---
+++ justin-core/src/test/scala/justin/db/kryo/DataSerializerTest.scala
@@ -0,0 +1,40 @@
+package justin.db.kryo
+
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.util.UUID
+
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.io.{Input, Output}
+import justin.consistent_hashing.NodeId
+import justin.db.Data
+import justin.vector_clocks.{Counter, VectorClock}
+import org.scalatest.{FlatSpec, Matchers}
+
+class DataSerializerTest extends FlatSpec with Matchers {
+
+  behavior of "Data Serializer"
+
+  it should "serialize/deserialize correctly" in {
+    // kryo init
+    val kryo = new Kryo()
+    kryo.register(classOf[justin.db.Data], DataSerializer)
+
+    // object
+    val vClock         = VectorClock[NodeId](Map(NodeId(1) -> Counter(3)))
+    val timestamp      = System.currentTimeMillis()
+    val serializedData = Data(id = UUID.randomUUID(), value = "some value", vClock, timestamp)
+
+    // serialize
+    val bos    = new ByteArrayOutputStream()
+    val output = new Output(bos)
+    val bytes = kryo.writeObject(output, serializedData)
+    output.flush()
+
+    // deserialize
+    val bis              = new ByteArrayInputStream(bos.toByteArray)
+    val ki               = new Input(bis)
+    val deserializedData = kryo.readObject(ki, classOf[Data])
+
+    serializedData shouldBe deserializedData
+  }
+}
