84973f72041ec1f19d2cb3ba3f4de8d4db96d2c2
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-04-05 10:15:58 +0200

    move cluster subscription logic to StorageNode actor body

--- justin-core/src/test/scala/justin/db/actors/StorageNodeActorTest.scala
+++ justin-core/src/test/scala/justin/db/actors/StorageNodeActorTest.scala
@@ -1,11 +1,34 @@
 package justin.db.actors
 
-import justin.consistent_hashing.NodeId
-import org.scalatest.{FlatSpec, Matchers}
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
+import com.typesafe.config.ConfigFactory
+import justin.consistent_hashing.{NodeId, Ring}
+import justin.db.actors.protocol.RegisterNode
+import justin.db.replica.N
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 
-class StorageNodeActorTest extends FlatSpec with Matchers {
+class StorageNodeActorTest extends TestKit(StorageNodeActorTest.actorSystem)
+  with FlatSpecLike
+  with ImplicitSender
+  with Matchers
+  with ScalaFutures
+  with BeforeAndAfterAll {
 
   behavior of "Storage Node Actor"
+
+  it should "send msg back with targeted NodeId when registration of other node has correctly happened" in {
+    // given
+    val nodeId = NodeId(1)
+    val testActor = TestActorRef(new TestActor(nodeId, Ring.apply(3, 1)))
+
+    // when
+    testActor ! RegisterNode(NodeId(2))
+
+    // then
+    expectMsg(RegisterNode(nodeId))
+  }
 
   it should "has defined role \"storagenode\"" in {
     StorageNodeActor.role shouldBe "storagenode"
@@ -16,5 +39,25 @@ class StorageNodeActorTest extends FlatSpec with Matchers {
     StorageNodeActor.name(NodeId(10))  shouldBe "id-10"
     StorageNodeActor.name(NodeId(20))  shouldBe "id-20"
     StorageNodeActor.name(NodeId(999)) shouldBe "id-999"
+  }
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  class TestActor(nodeId: NodeId, ring: Ring) extends StorageNodeActor(nodeId, null, ring, N(1))
+}
+
+object StorageNodeActorTest {
+  def actorSystem: ActorSystem = {
+    val config = ConfigFactory.parseString(
+      """
+        |akka.loglevel = off
+        |akka.actor.provider = cluster
+        |akka.cluster.auto-join = off
+        |akka.cluster.metrics.enabled = off
+      """.stripMargin).withFallback(ConfigFactory.load())
+
+    ActorSystem("test-system", config)
   }
 }
