b8b0f18bef89b20b1525cf051e248091c5024385
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-04-06 06:31:50 +0200

    test of serialization/deserialization of StorageNodeNotFoundRead

--- justin-core/src/test/scala/justin/db/kryo/StorageNodeReadResponseSerializerTest.scala
+++ justin-core/src/test/scala/justin/db/kryo/StorageNodeReadResponseSerializerTest.scala
@@ -7,7 +7,7 @@ import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.{Input, Output}
 import justin.consistent_hashing.NodeId
 import justin.db.Data
-import justin.db.actors.protocol.StorageNodeFoundRead
+import justin.db.actors.protocol.{StorageNodeFoundRead, StorageNodeNotFoundRead}
 import justin.vector_clocks.{Counter, VectorClock}
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -39,6 +39,28 @@ class StorageNodeReadResponseSerializerTest extends FlatSpec with Matchers {
     val bis              = new ByteArrayInputStream(bos.toByteArray)
     val input            = new Input(bis)
     val deserializedData = kryo.readObject(input, classOf[StorageNodeFoundRead])
+
+    serializedData shouldBe deserializedData
+  }
+
+  it should "serialize/deserialize StorageNodeNotFoundRead" in {
+    // kryo init
+    val kryo = new Kryo()
+    kryo.register(classOf[StorageNodeNotFoundRead], StorageNodeReadResponseSerializer)
+
+    // object
+    val serializedData = StorageNodeNotFoundRead(UUID.randomUUID())
+
+    // serialization
+    val bos    = new ByteArrayOutputStream()
+    val output = new Output(bos)
+    val bytes  = kryo.writeObject(output, serializedData)
+    output.flush()
+
+    // deserialization
+    val bis              = new ByteArrayInputStream(bos.toByteArray)
+    val input            = new Input(bis)
+    val deserializedData = kryo.readObject(input, classOf[StorageNodeNotFoundRead])
 
     serializedData shouldBe deserializedData
   }
