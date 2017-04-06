163f1a1f2384931fe394491940a0824e498d0940
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-04-06 06:34:53 +0200

    test of serialization/deserialization of StorageNodeConflictedRead

--- justin-core/src/test/scala/justin/db/kryo/StorageNodeReadResponseSerializerTest.scala
+++ justin-core/src/test/scala/justin/db/kryo/StorageNodeReadResponseSerializerTest.scala
@@ -7,7 +7,7 @@ import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.{Input, Output}
 import justin.consistent_hashing.NodeId
 import justin.db.Data
-import justin.db.actors.protocol.{StorageNodeFailedRead, StorageNodeFoundRead, StorageNodeNotFoundRead}
+import justin.db.actors.protocol.{StorageNodeConflictedRead, StorageNodeFailedRead, StorageNodeFoundRead, StorageNodeNotFoundRead}
 import justin.vector_clocks.{Counter, VectorClock}
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -83,6 +83,40 @@ class StorageNodeReadResponseSerializerTest extends FlatSpec with Matchers {
     val bis              = new ByteArrayInputStream(bos.toByteArray)
     val input            = new Input(bis)
     val deserializedData = kryo.readObject(input, classOf[StorageNodeFailedRead])
+
+    serializedData shouldBe deserializedData
+  }
+
+  it should "serialize/deserialize StorageNodeConflictedRead" in {
+    // kryo init
+    val kryo = new Kryo()
+    kryo.register(classOf[StorageNodeConflictedRead], StorageNodeReadResponseSerializer)
+
+    // object
+    val data1 = Data(
+      id        = UUID.randomUUID(),
+      value     = "some value 1",
+      vclock    = VectorClock[NodeId](Map(NodeId(1) -> Counter(3))),
+      timestamp = System.currentTimeMillis()
+    )
+    val data2 = Data(
+      id        = UUID.randomUUID(),
+      value     = "some value 2",
+      vclock    = VectorClock[NodeId](Map(NodeId(1) -> Counter(1))),
+      timestamp = System.currentTimeMillis()
+    )
+    val serializedData = StorageNodeConflictedRead(List(data1, data2))
+
+    // serialization
+    val bos    = new ByteArrayOutputStream()
+    val output = new Output(bos)
+    val bytes  = kryo.writeObject(output, serializedData)
+    output.flush()
+
+    // deserialization
+    val bis              = new ByteArrayInputStream(bos.toByteArray)
+    val input            = new Input(bis)
+    val deserializedData = kryo.readObject(input, classOf[StorageNodeConflictedRead])
 
     serializedData shouldBe deserializedData
   }
