5e4770ac21528993d6ad34d5b94425a770ef551a
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-04-19 09:03:57 +0200

    move from replica to db pacakge

--- justin-core/src/test/scala/justin/db/DataTest.scala
+++ justin-core/src/test/scala/justin/db/DataTest.scala
@@ -1,9 +1,9 @@
-package justin.db.replica
+package justin.db
 
 import java.util.UUID
 
 import justin.consistent_hashing.NodeId
-import justin.db.Data
+import justin.db.replica.PreferenceList
 import justin.vector_clocks.{Counter, VectorClock}
 import org.scalatest.{FlatSpec, Matchers}
 
