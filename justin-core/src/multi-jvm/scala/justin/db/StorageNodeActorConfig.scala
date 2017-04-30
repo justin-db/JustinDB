ab79d998813c233237f674ee84f49d4288b4a132
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-04-30 09:36:52 +0200

    run StorageActor on every JVM ndoe during forming cluster test

--- justin-core/src/multi-jvm/scala/justin/db/StorageNodeActorConfig.scala
+++ justin-core/src/multi-jvm/scala/justin/db/StorageNodeActorConfig.scala
@@ -4,12 +4,12 @@ import akka.remote.testconductor.RoleName
 import akka.remote.testkit.MultiNodeConfig
 import com.typesafe.config.ConfigFactory
 
-object StorageNodeActorConfig extends MultiNodeConfig {
+case class StorageNodeActorConfig(clusterName: String) extends MultiNodeConfig {
   val seed: RoleName  = role("storagenode-seed")
   val node1: RoleName = role("storagenode-1")
   val node2: RoleName = role("storagenode-2")
 
-  commonConfig(ConfigFactory.parseString("""
+  commonConfig(ConfigFactory.parseString(s"""
     akka {
       loggers = ["akka.event.slf4j.Slf4jLogger"]
       loglevel = "DEBUG"
@@ -22,7 +22,8 @@ object StorageNodeActorConfig extends MultiNodeConfig {
     akka.netty.tcp.hostname = "127.0.0.1"
 
     akka.cluster.roles = [storagenode]
-    akka.cluster.seed-nodes = [ "akka.tcp://justin-db-cluster-system@127.0.0.1:2551", "akka.tcp://justin-db-cluster-system@127.0.0.1:2552" ]
+    akka.cluster.role.storagenode.min-nr-of-members = 3
+    akka.cluster.seed-nodes = [ "akka.tcp://$clusterName@localhost:2551", "akka.tcp://$clusterName@localhost:2552" ]
 
     # don't use sigar for tests, native lib not in path
     akka.cluster.metrics.collector-class = akka.cluster.JmxMetricsCollector"""
@@ -32,21 +33,18 @@ object StorageNodeActorConfig extends MultiNodeConfig {
     ConfigFactory.parseString(
       """
         akka.remote.netty.tcp.port=2551
-        node.id=1
       """.stripMargin)
   )
   nodeConfig(node1)(
     ConfigFactory.parseString(
       """
         akka.remote.netty.tcp.port=2552
-        node.id=2
       """.stripMargin)
   )
   nodeConfig(node2)(
     ConfigFactory.parseString(
       """
         akka.remote.netty.tcp.port=0
-        node.id=3
       """.stripMargin)
   )
 }
--- justin-core/src/multi-jvm/scala/justin/db/StorageNodeActorSpec.scala
+++ justin-core/src/multi-jvm/scala/justin/db/StorageNodeActorSpec.scala
@@ -1,21 +1,25 @@
 package justin.db
 
-import akka.cluster.Cluster
-import akka.cluster.ClusterEvent.{CurrentClusterState, MemberUp}
 import akka.remote.testkit.MultiNodeSpec
 import akka.testkit.ImplicitSender
+import justin.consistent_hashing.{NodeId, Ring}
+import justin.db.actors.{StorageNodeActor, StorageNodeActorRef}
+import justin.db.client.ActorRefStorageNodeClient
+import justin.db.replica.N
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 class StorageNodeActorSpecMultiJvmNode1 extends StorageNodeActorSpec
 class StorageNodeActorSpecMultiJvmNode2 extends StorageNodeActorSpec
 class StorageNodeActorSpecMultiJvmNode3 extends StorageNodeActorSpec
 
-class StorageNodeActorSpec extends MultiNodeSpec(StorageNodeActorConfig)
+class StorageNodeActorSpec extends MultiNodeSpec(StorageNodeActorConfig("StorageNodeActorSpec"))
   with ScalaTestMultiNodeSpec
   with ImplicitSender {
 
-  import StorageNodeActorConfig._
+  private val config = StorageNodeActorConfig("StorageNodeActorSpec")
+  import config._
 
   override def initialParticipants: Int = roles.size
 
@@ -27,18 +31,33 @@ class StorageNodeActorSpec extends MultiNodeSpec(StorageNodeActorConfig)
 
   "Storage Node Actors" must {
 
+    "wait for all nodes to enter a barrier" in {
+      enterBarrier("startup")
+    }
+
     "form the cluster" in within(10.seconds) {
+      runOn(seed) {
+        val storageNodeActor1 = storageNodeActorRef(NodeId(0))
+        enterBarrier("deployed")
+      }
+      runOn(node1) {
+        val storageNodeActor2 = storageNodeActorRef(NodeId(1))
+        enterBarrier("deployed")
 
-      Cluster(system).subscribe(testActor, classOf[MemberUp])
-      expectMsgClass(classOf[CurrentClusterState])
-
-      Cluster(system).join(seedStorageNodeAddress)
-
-      receiveN(3).map { case MemberUp(m) => m.address }.toSet shouldBe Set(seedStorageNodeAddress, storageNode1Address, storageNode2Address)
-
-      Cluster(system).unsubscribe(testActor)
+      }
+      runOn(node2) {
+        val storageNodeActor3 = storageNodeActorRef(NodeId(2))
+        enterBarrier("deployed")
+      }
 
       enterBarrier("cluster-up")
     }
   }
+
+  private def storageNodeActorRef(nodeId: NodeId) = new ActorRefStorageNodeClient(StorageNodeActorRef {
+    system.actorOf(
+      props = StorageNodeActor.props(nodeId, null, Ring.apply(nodesSize = 3, partitionsSize =  21), N(3)),
+      name  = StorageNodeActor.name(nodeId)
+    )
+  })
 }
