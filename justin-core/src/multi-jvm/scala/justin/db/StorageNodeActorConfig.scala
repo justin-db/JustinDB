4e0e1ff6d955babc83b39d18a5788288455797c3
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-04-30 08:38:03 +0200

    define config per jvm node

--- justin-core/src/multi-jvm/scala/justin/db/StorageNodeActorConfig.scala
+++ justin-core/src/multi-jvm/scala/justin/db/StorageNodeActorConfig.scala
@@ -5,13 +5,45 @@ import akka.remote.testkit.MultiNodeConfig
 import com.typesafe.config.ConfigFactory
 
 object StorageNodeActorConfig extends MultiNodeConfig {
-  val seed: RoleName  = role("StorageNode-seed")
-  val node1: RoleName = role("StorageNode-1")
-  val node2: RoleName = role("StorageNode-2")
+  val seed: RoleName  = role("storagenode-seed")
+  val node1: RoleName = role("storagenode-1")
+  val node2: RoleName = role("storagenode-2")
 
   commonConfig(ConfigFactory.parseString("""
-    akka.actor.provider="akka.cluster.ClusterActorRefProvider"
+    akka {
+      loggers = ["akka.event.slf4j.Slf4jLogger"]
+      loglevel = "DEBUG"
+      logging-filter = "akka.event.slf4j.Slf4jLoggingFilter"
+      log-dead-letters-during-shutdown = off
+      remote.log-remote-lifecycle-events = off
+    }
+    akka.actor.provider = "cluster"
+
+    akka.netty.tcp.hostname = "127.0.0.1"
+
     # don't use sigar for tests, native lib not in path
     akka.cluster.metrics.collector-class = akka.cluster.JmxMetricsCollector"""
   ))
+
+  nodeConfig(seed)(
+    ConfigFactory.parseString(
+      """
+        akka.remote.netty.tcp.port=25521
+        node.id=1
+      """.stripMargin)
+  )
+  nodeConfig(node1)(
+    ConfigFactory.parseString(
+      """
+        akka.remote.netty.tcp.port=25522
+        node.id=2
+      """.stripMargin)
+  )
+  nodeConfig(node2)(
+    ConfigFactory.parseString(
+      """
+        akka.remote.netty.tcp.port=25523
+        node.id=3
+      """.stripMargin)
+  )
 }
