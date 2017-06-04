3cc0cf891fa84aa7602a00c7bd5e448a09644bcb
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-06-04 10:12:11 +0200

    clean Dependency config

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,6 @@ object Version {
   val scalatest    = "3.0.3"
   val scalacheck   = "1.13.5"
   val sigarLoader  = "1.6.6"
-  val scopt        = "3.5.0"
   val logback      = "1.2.3"
   val scalaLogging = "3.5.0"
   val constructR   = "0.17.0"
@@ -21,39 +20,37 @@ object Version {
 
 object Library {
   val akkaActor            = "com.typesafe.akka"     %% "akka-actor"                    % Version.akka
-  val akkaSfl4j            = "com.typesafe.akka"     %% "akka-slf4j"                    % Version.akka
   val akkaTestkit          = "com.typesafe.akka"     %% "akka-testkit"                  % Version.akka
+  val akkaRemote           = "com.typesafe.akka"     %% "akka-remote"                   % Version.akka
+  val akkaCluster          = "com.typesafe.akka"     %% "akka-cluster"                  % Version.akka
+  val akkaClusterMetrics   = "com.typesafe.akka"     %% "akka-cluster-metrics"          % Version.akka
+  val akkaClusterTools     = "com.typesafe.akka"     %% "akka-cluster-tools"            % Version.akka
+  val akkaStream           = "com.typesafe.akka"     %% "akka-stream"                   % Version.akka
+  val akkaMultiNodeTestkit = "com.typesafe.akka"     %% "akka-multi-node-testkit"       % Version.akka
+  val akkaHttp             = "com.typesafe.akka"     %% "akka-http"                     % Version.akkaHttp
+  val akkaHttpSprayJson    = "com.typesafe.akka"     %% "akka-http-spray-json"          % Version.akkaHttp
+  val akkaHttpTestkit      = "com.typesafe.akka"     %% "akka-http-testkit"             % Version.akkaHttp
   val akkaKryo             = "com.github.romix.akka" %% "akka-kryo-serialization"       % Version.akkaKryo
-
-  // http
-  val akkaHttp             = "com.typesafe.akka" %% "akka-http"                         % Version.akkaHttp
-  val akkaHttpSprayJson    = "com.typesafe.akka" %% "akka-http-spray-json"              % Version.akkaHttp
-  val akkaHttpTestkit      = "com.typesafe.akka" %% "akka-http-testkit"                 % Version.akkaHttp
-  val akkaStream           = "com.typesafe.akka" %% "akka-stream"                       % Version.akka
-
-  // cluster
-  val akkaRemote           = "com.typesafe.akka" %% "akka-remote"                       % Version.akka
-  val akkaCluster          = "com.typesafe.akka" %% "akka-cluster"                      % Version.akka
-  val akkaClusterMetrics   = "com.typesafe.akka" %% "akka-cluster-metrics"              % Version.akka
-  val akkaClusterTools     = "com.typesafe.akka" %% "akka-cluster-tools"                % Version.akka
-  val akkaMultiNodeTestkit = "com.typesafe.akka" %% "akka-multi-node-testkit"           % Version.akka
+  val kamonSigar           = "io.kamon"               % "sigar-loader"                  % Version.sigarLoader
 
   // test
   val scalactic            = "org.scalactic"     %% "scalactic"                         % Version.scalatest
   val scalatest            = "org.scalatest"     %% "scalatest"                         % Version.scalatest
   val scalacheck           = "org.scalacheck"    %% "scalacheck"                        % Version.scalacheck
 
-  // other
-  val kamonSigar           = "io.kamon"                    % "sigar-loader"             % Version.sigarLoader
-  val scopt                = "com.github.scopt"           %% "scopt"                    % Version.scopt
+  // logging
+  val akkaSfl4j            = "com.typesafe.akka"          %% "akka-slf4j"               % Version.akka
   val logback              = "ch.qos.logback"              % "logback-classic"          % Version.logback
   val scalaLogging         = "com.typesafe.scala-logging" %% "scala-logging"            % Version.scalaLogging
-  val configAnnotation     = "com.wacai"                  %% "config-annotation"        % Version.configAnnotation
-  val macroParadise        = "org.scalamacros"            % "paradise"                  % Version.macroParadise
+
+  // config
+  val configAnnotation     = "com.wacai"         %% "config-annotation"                 % Version.configAnnotation
+  val macroParadise        = "org.scalamacros"    % "paradise"                          % Version.macroParadise
 
   // server side events
   val akkaSse              = "de.heikoseeberger" %% "akka-sse"                          % Version.akkaSse
 
+  // service discovery
   val constructR           = "de.heikoseeberger" %% "constructr"                        % Version.constructR
   val constructRetcd       = "de.heikoseeberger" %% "constructr-coordination-etcd"      % Version.constructR
 }
@@ -63,15 +60,15 @@ object Dependencies {
 
   private val genericTest = Seq(scalactic, scalatest % "test")
 
-  private val akkaCommon        = Seq(akkaActor, akkaSfl4j, akkaTestkit, akkaKryo)
-  private val akkaHttpCommon    = Seq(akkaHttp, akkaHttpSprayJson, akkaHttpTestkit, akkaStream)
+  private val akkaCommon        = Seq(akkaActor, akkaSfl4j, akkaTestkit, akkaKryo, akkaStream)
+  private val akkaHttpCommon    = Seq(akkaHttp, akkaHttpSprayJson, akkaHttpTestkit)
   private val akkaClusterCommon = Seq(akkaRemote, akkaMultiNodeTestkit % "multi-jvm", akkaCluster, akkaClusterMetrics, akkaClusterTools, kamonSigar)
 
   private val constructr = Seq(constructR, constructRetcd)
 
   val core = akkaCommon ++ akkaClusterCommon ++ genericTest ++ Seq(scalacheck % "test", logback, scalaLogging) ++ Seq(akkaHttpSprayJson)
 
-  val httpApi = akkaCommon ++ akkaHttpCommon ++ genericTest ++ Seq(scopt) ++ Seq(akkaSse)
+  val httpApi = akkaCommon ++ akkaHttpCommon ++ genericTest ++ Seq(akkaSse)
 
   val storageApi = genericTest
   val storageInMem = genericTest
