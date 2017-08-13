3b976bb6ea46066eea66773e9523f45e20440078
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-08-13 10:07:34 +0200

    place deployment recipies under dedicated repository (JustinDB-deployment)

--- docker/99-tear-down-local-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ex
-
-docker stop etcd && docker rm etcd
-
-for ID in {0..2}; do
-    docker-machine stop justindb-$ID && \
-    docker-machine rm -y justindb-$ID
-done
--- docker/docker-compose-down.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-docker stop etcd && docker rm etcd
-docker stop justindb-1 && docker rm justindb-1
-docker stop justindb-2 && docker rm justindb-2
-docker stop justindb-3 && docker rm justindb-3
