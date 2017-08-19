72cbda69dc116a85859d441c47e6a4261194b96a
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-08-19 12:51:31 +0200

    integrate PersistentStorage with LogDB

--- justin-storage-persistent/src/main/scala/justin/db/storage/PersistentStorage.scala
+++ justin-storage-persistent/src/main/scala/justin/db/storage/PersistentStorage.scala
@@ -1,14 +1,31 @@
 package justin.db.storage
 
+import java.io.File
 import java.util.UUID
-import justin.db.storage.PluggableStorageProtocol.{Ack, DataOriginality, StorageGetData, StoragePutData}
 
-import scala.concurrent.{ExecutionContext, Future}
+import justin.db.storage.PluggableStorageProtocol.{Ack, DataOriginality, StorageGetData, StoragePutData}
+import logdb.LogDB
+
+import scala.concurrent.Future
 
 class PersistentStorage extends PluggableStorageProtocol {
-  println("PERSISTENT STORAGE LOADED")
+  println("PERSISTENT-STORAGE LOADED")
 
-  override def get(id: UUID)(resolveOriginality: (UUID) => DataOriginality): Future[StorageGetData] = ???
+  private[this] val logDB = {
+    val journalPath: String = "/opt/docker/justindb/journal" // TODO: make it configurable
+    val file: File = new File(journalPath)
+    file.createNewFile()
+    new LogDB(file)(new JustinLogDBSerializer)
+  }
 
-  override def put(cmd: StoragePutData)(resolveOriginality: (UUID) => DataOriginality): Future[Ack] = ???
+  override def get(id: UUID)(resolveOriginality: (UUID) => DataOriginality): Future[StorageGetData] = Future.successful {
+    logDB.get(id)
+      .map(StorageGetData.Single)
+      .getOrElse(StorageGetData.None)
+  }
+
+  override def put(cmd: StoragePutData)(resolveOriginality: (UUID) => DataOriginality): Future[Ack] = {
+    logDB.save(cmd.data.id, cmd.data)
+    Ack.future
+  }
 }
