3b82ee58177d3a873b62a3a19a4e1e36259d4966
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-08-19 10:08:06 +0200

    add helper methods to LogDB serializer

--- justin-storage-persistent/src/main/scala/logdb/serializer/LogDBSerializer.scala
+++ justin-storage-persistent/src/main/scala/logdb/serializer/LogDBSerializer.scala
@@ -1,6 +1,8 @@
 package logdb.serializer
 
 import java.io.{ByteArrayOutputStream, DataOutputStream, RandomAccessFile}
+import java.nio.ByteBuffer
+import java.util.UUID
 
 trait LogDBSerializer[K, V] {
   protected def writeKey(key: K): Array[Byte]
@@ -37,5 +39,37 @@ trait LogDBSerializer[K, V] {
     raf.read(bufferValue, 0, sizeValue)
 
     (readKey(bufferKey), readValue(bufferValue))
+  }
+
+  protected def uuid2bytes(uuid: UUID): Array[Byte] = {
+    val bb = ByteBuffer.wrap(new Array[Byte](16))
+    bb.putLong(uuid.getMostSignificantBits)
+    bb.putLong(uuid.getLeastSignificantBits)
+    bb.array()
+  }
+  protected def bytes2uuid(bytes: Array[Byte]): UUID = {
+    val bb = ByteBuffer.wrap(bytes)
+    val firstLong = bb.getLong
+    val secondLong = bb.getLong
+    new UUID(firstLong, secondLong)
+  }
+
+  protected def int2bytes(int: Int): Array[Byte] = {
+    ByteBuffer.allocate(4).putInt(int).array()
+  }
+  protected def bytes2int(bytes: Array[Byte]): Int = {
+    ByteBuffer.wrap(bytes).getInt()
+  }
+
+  protected def long2bytes(x: Long): Array[Byte] = {
+    val buffer = ByteBuffer.allocate(8)
+    buffer.putLong(x)
+    buffer.array
+  }
+  protected def bytes2long(bytes: Array[Byte]): Long = {
+    val buffer = ByteBuffer.allocate(8)
+    buffer.put(bytes)
+    buffer.flip //need flip
+    buffer.getLong
   }
 }
