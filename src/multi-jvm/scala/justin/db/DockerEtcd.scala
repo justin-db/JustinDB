651e0fe5a9fd7afea838f44dc834ce89a8d6f7a6
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-11-05 21:20:41 +0100

    set etcd envs

--- src/multi-jvm/scala/justin/db/DockerEtcd.scala
+++ src/multi-jvm/scala/justin/db/DockerEtcd.scala
@@ -8,6 +8,7 @@ trait DockerEtcd extends DockerKitDockerJava {
 
   val etcdContainer: DockerContainer = DockerContainer("quay.io/coreos/etcd:v2.3.7", name = Some("etcd"))
     .withPorts(DefaultEtcdPort -> Some(DefaultEtcdPort))
+    .withEnv(s"ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:$DefaultEtcdPort", s"ETCD_ADVERTISE_CLIENT_URLS=http://0.0.0.0:$DefaultEtcdPort")
     .withReadyChecker(DockerReadyChecker.LogLineContains("set the initial cluster version"))
 
   abstract override def dockerContainers: List[DockerContainer] = etcdContainer :: super.dockerContainers
