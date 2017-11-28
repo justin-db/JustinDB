80956c645f0f597892b1d5f2837918953ee5106a
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-11-28 21:10:14 +0100

    run real justindb process in multi-jvm tests

--- src/multi-jvm/scala/justin/db/ConvergeJustinDBClusterSpec.scala
+++ src/multi-jvm/scala/justin/db/ConvergeJustinDBClusterSpec.scala
@@ -1,114 +1,58 @@
 package justin.db
 
-import java.util.UUID
-
-import akka.actor.ActorRef
-import akka.pattern.ask
 import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec}
-import akka.util.Timeout
-import justin.db.actors.StorageNodeActor
-import justin.db.consistenthashing.{NodeId, Ring}
-import justin.db.replica.N
-import justin.db.storage.{JustinData, PluggableStorageProtocol}
-import akka.testkit.TestDuration
-import justin.db.cluster.ClusterMembers
-import justin.db.cluster.datacenter.Datacenter
+import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
-
-final class ConvergeJustinDBClusterConfig extends MultiNodeConfig {
+final class ConvergeJustinDBClusterConfig extends MultiNodeConfig with DockerEtcd {
   val first  = role("first")
   val second = role("second")
   val third  = role("third")
-  val fourth = role("fourth")
-  val fifth  = role("fifth")
 
-  commonConfig(MultiNodeClusterSpec.clusterConfig)
+  private[this] val allRoles  = List(first, second, third)
+  private[this] val clusterName = "ConvergeJustinDBClusterSpec"
+  private[this] val commonBaseConfig = ConfigFactory.parseString(
+    s"""
+       |akka.loglevel = INFO
+       |akka.log-dead-letters = off
+       |akka.log-dead-letters-during-shutdown = off
+       |akka.remote.log-remote-lifecycle-events = off
+    """.stripMargin
+  )
+  private[this] def commonNodeConfig(id: Int) = ConfigFactory.parseString(
+    s"""
+       |justin.system = $clusterName
+       |justin.node-id = $id
+       |justin.http.port = ${9000 + id}
+       |akka.cluster.role.storagenode.min-nr-of-members = ${allRoles.size}
+       |akka.remote.netty.tcp.port = 0
+       |akka.remote.netty.tcp.hostname = "localhost"
+       |akka.remote.netty.tcp.bind-hostname = "0.0.0.0"
+       |akka.remote.netty.tcp.bind-port = 0
+       |akka.cluster.http.management.port = ${19999 + id}
+    """.stripMargin
+  )
+
+  commonConfig(commonBaseConfig.withFallback(JustinDBConfig.init.config))
+
+  allRoles.zipWithIndex.foreach { case (roleName, id) =>
+    nodeConfig(roleName)(commonNodeConfig(id))
+  }
 }
 
 final class ConvergeJustinDBClusterSpecMultiJvmNode1 extends ConvergeJustinDBClusterSpec
 final class ConvergeJustinDBClusterSpecMultiJvmNode2 extends ConvergeJustinDBClusterSpec
 final class ConvergeJustinDBClusterSpecMultiJvmNode3 extends ConvergeJustinDBClusterSpec
-final class ConvergeJustinDBClusterSpecMultiJvmNode4 extends ConvergeJustinDBClusterSpec
-final class ConvergeJustinDBClusterSpecMultiJvmNode5 extends ConvergeJustinDBClusterSpec
 
 abstract class ConvergeJustinDBClusterSpec(config: ConvergeJustinDBClusterConfig)
   extends MultiNodeSpec(config)
   with MultiNodeClusterSpec {
 
-  import config._
-
   def this() = this(new ConvergeJustinDBClusterConfig())
 
-  "A cluster of 5 members" must {
+  "ConstructR should manage an Akka cluster" in {
+    val config = new JustinDBConfig(system.settings.config)
+    val justinDB = JustinDB.init(config)
 
-    "setup stable akka cluster" in {
-      awaitConstructRClusterUp()
-      enterBarrier("akka-cluster-up")
-    }
-
-    "reach final convergence" in within(50.seconds) {
-      runOn(first) {
-        val ref = storageNodeActor(0)
-        enterBarrier("storage-node-actor-up")
-
-        awaitMembers(ref)
-        enterBarrier("cluster-converged")
-      }
-      runOn(second) {
-        val ref = storageNodeActor(1)
-        enterBarrier("storage-node-actor-up")
-
-        awaitMembers(ref)
-        enterBarrier("cluster-converged")
-      }
-      runOn(third) {
-        val ref = storageNodeActor(2)
-        enterBarrier("storage-node-actor-up")
-
-        awaitMembers(ref)
-        enterBarrier("cluster-converged")
-      }
-      runOn(fourth) {
-        val ref = storageNodeActor(3)
-        enterBarrier("storage-node-actor-up")
-
-        awaitMembers(ref)
-        enterBarrier("cluster-converged")
-      }
-      runOn(fifth) {
-        val ref = storageNodeActor(4)
-        enterBarrier("storage-node-actor-up")
-
-        awaitMembers(ref)
-        enterBarrier("cluster-converged")
-      }
-    }
-  }
-
-  private[this] def awaitMembers(ref: ActorRef) = awaitAssert(
-    a = {
-      implicit val timeout = Timeout(1.second.dilated)
-      val members = Await.result((ref ? "members").mapTo[ClusterMembers], 1.second.dilated)
-      members.size shouldBe initialParticipants
-    },
-    interval = 2.seconds
-  )
-
-  private[this] def storageNodeActor(nodeId: Int) = {
-    val storage = new PluggableStorageProtocol {
-      override def get(id: UUID)(resolveOriginality: (UUID) => PluggableStorageProtocol.DataOriginality): Future[PluggableStorageProtocol.StorageGetData] = ???
-      override def put(data: JustinData)(resolveOriginality: (UUID) => PluggableStorageProtocol.DataOriginality): Future[PluggableStorageProtocol.Ack] = ???
-    }
-    val ring = Ring(nodesSize = initialParticipants, partitionsSize = 100)
-    val n = N(3)
-    val nodeid = NodeId(nodeId)
-    val datacenter = Datacenter("default")
-
-    system.actorOf(
-      props = StorageNodeActor.props(nodeid, datacenter, storage, ring, n),
-      name  = StorageNodeActor.name(nodeid, datacenter)
-    )
+    enterBarrier("akka-cluster-up")
   }
 }
