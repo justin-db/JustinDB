176d021b665b4d69faf94eb06347d361628d3c45
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-11-28 21:45:17 +0100

    redefine MultiDCSpec

--- src/multi-jvm/scala/justin/db/MultiDcSpec.scala
+++ src/multi-jvm/scala/justin/db/MultiDcSpec.scala
@@ -1,62 +1,60 @@
 package justin.db
 
-import akka.cluster.MemberStatus
 import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec}
 import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.duration._
-
-final class MultiDcSpecConfig(crossDcConnections: Int = 5) extends MultiNodeConfig {
+final class MultiDcSpecConfig(crossDcConnections: Int = 1) extends MultiNodeConfig {
   val first  = role("first")
   val second = role("second")
-  val third  = role("third")
-  val fourth = role("fourth")
-  val fifth  = role("fifth")
 
-  commonConfig(ConfigFactory.parseString(
+  private[this] val allRoles  = List(first, second)
+  private[this] val clusterName = "MultiDcSpec"
+  private[this] val commonBaseConfig = ConfigFactory.parseString(
     s"""
-      akka.cluster.multi-data-center.cross-data-center-connections = $crossDcConnections
-    """).withFallback(MultiNodeClusterSpec.clusterConfig)
+       |akka.loglevel = INFO
+       |akka.log-dead-letters = off
+       |akka.log-dead-letters-during-shutdown = off
+       |akka.remote.log-remote-lifecycle-events = off
+    """.stripMargin
+  )
+  private[this] def commonNodeConfig(id: Int) = ConfigFactory.parseString(
+    s"""
+       |justin.system = $clusterName
+       |justin.node-id = $id
+       |justin.http.port = ${9000 + id}
+       |akka.cluster.role.storagenode.min-nr-of-members = ${allRoles.size}
+       |akka.remote.netty.tcp.port = 0
+       |akka.remote.netty.tcp.hostname = "localhost"
+       |akka.remote.netty.tcp.bind-hostname = "0.0.0.0"
+       |akka.remote.netty.tcp.bind-port = 0
+       |akka.cluster.http.management.port = ${19999 + id}
+       |akka.cluster.multi-data-center.cross-data-center-connections = $crossDcConnections
+       |akka.cluster.multi-data-center.self-data-center = "dc$id"
+    """.stripMargin
   )
 
-  nodeConfig(first, second)(ConfigFactory.parseString(
-    """
-      akka.cluster.multi-data-center.self-data-center = "dc1"
-    """))
+  commonConfig(commonBaseConfig.withFallback(JustinDBConfig.init.config))
 
-  nodeConfig(third, fourth, fifth)(ConfigFactory.parseString(
-    """
-      akka.cluster.multi-data-center.self-data-center = "dc2"
-    """))
+  allRoles.zipWithIndex.foreach { case (roleName, id) =>
+    nodeConfig(roleName)(commonNodeConfig(id))
+  }
 }
 
 final class MultiDcMultiJvm1 extends MultiDcSpec
 final class MultiDcMultiJvm2 extends MultiDcSpec
-final class MultiDcMultiJvm3 extends MultiDcSpec
-final class MultiDcMultiJvm4 extends MultiDcSpec
-final class MultiDcMultiJvm5 extends MultiDcSpec
 
 abstract class MultiDcSpec(config: MultiDcSpecConfig)
   extends MultiNodeSpec(config)
     with MultiNodeClusterSpec {
 
-  import config._
-
   def this() = this(new MultiDcSpecConfig())
 
   "A cluster with multiple data centers" must {
     "be able to form" in {
+      val config = new JustinDBConfig(system.settings.config)
+      val justinDB = JustinDB.init(config)
 
-      awaitConstructRClusterUp()
-      enterBarrier("form-cluster-join-attempt")
-
-      runOn(first, second, third, fourth, fifth) {
-        within(20.seconds) {
-          awaitAssert(cluster.state.members.filter(_.status == MemberStatus.Up) should have size (initialParticipants))
-        }
-      }
-
-      enterBarrier("cluster started")
+      enterBarrier("justindb-cluster-up")
     }
   }
 }
